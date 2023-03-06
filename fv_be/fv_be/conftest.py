98cca0c8cae4e52942c2d18f00dd648cc9dc1747
Sahil Sharma <sahil.sharma071997@gmail.com>
2023-03-06 10:45:48 -0800

    FW-4115 project skeleton for Django app

---
+++ fv_be/fv_be/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from fv_be.users.models import User
+from fv_be.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
