a9ee36ec745fb17e0717da6dc2f2bc12435b7813
Sahil Sharma <sahil.sharma071997@gmail.com>
2023-03-20 15:28:08 -0700

    combined id and lable into title field for parts of speech model

--- fv_be/app/models/part_of_speech.py
+++ fv_be/app/models/part_of_speech.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 # FirstVoices
 from .base import BaseModel
@@ -8,16 +9,11 @@ class PartOfSpeech(BaseModel):
     """Model for Categories."""
 
     # Fields
-    # todo: name for the following field, can we put title ? or will it be very similar to label ?
-    # since other models have a title field
-    name = models.CharField(max_length=200)
-    # from wiki: name of the part of speech in snake case e.g.intransitive_verb
-    label = models.CharField(max_length=200)
-    # todo: name for the following field
+    title = models.CharField(max_length=200)
     parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.PROTECT)
 
     class Meta:
-        verbose_name_plural = "PartsOfSpeech"
+        verbose_name_plural = _("PartsOfSpeech")
 
     def __str__(self):
         return self.name
