0669b62be9dbd54b44335f447d75024d8eae9c7c
Sahil Sharma <sahil.sharma071997@gmail.com>
2023-03-22 16:20:33 -0700

    Fw 4053 create dictionary data models (#12)
    * Added initial dictionary associated models
    
    * added dictionary models to admin
    made related dictionary items field optional
    
    * Added i18n support, added BaseModel to dictionary classes
    
    * Added category model, added parts_of_speech model, added foreign keys to models
    
    * Added related names, modified relationships b/w models, added through table
    
    * Made foreign keys to self nullable in category and parts_of_speech models
    
    * changed choices to Django ENUM model
    
    * Changed spelling of Acknowledgement
    
    * combined id and lable into title field for parts of speech model
    
    * Corrected spelling for parts of speech comment
    
    * Added max 1 level nesting rule in categories
    
    * Added site related base models instead of base model in dictionary models
    
    * - Added missing related name to foreign key relations
    - Changed max length for custom_order in DicitonaryEntry
    - Changed name of the through table for DictionaryEntry self relation
    
    * - Added verbose name to dictionary models
    - Rephrase validationError message for category nesting rule
    -
    
    * - Added migration field for batch_id
    - Changed names for notes, translation, ack models
    
    * - Added validation to partOfSpeech model for no grandparents
    - Added initial migration file
    
    * Updated migration file

--- fv_be/app/admin/admin.py
+++ fv_be/app/admin/admin.py
@@ -5,6 +5,16 @@ from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site as Sites
 from rest_framework.authtoken.models import TokenProxy
 
+from fv_be.app.models.category import Category
+from fv_be.app.models.dictionary import (
+    DictionaryAcknowledgement,
+    AlternateSpelling,
+    DictionaryEntry,
+    DictionaryNote,
+    Pronunciation,
+    DictionaryTranslation,
+)
+from fv_be.app.models.part_of_speech import PartOfSpeech
 from fv_be.app.models.sites import Site
 
 from .base_admin import BaseAdmin
@@ -15,6 +25,14 @@ from .characters_admin import (
 )
 from .sites_admin import MembershipInline, SiteFeatureInline
 
+dictionary_models = [
+    DictionaryNote,
+    DictionaryAcknowledgement,
+    DictionaryTranslation,
+    AlternateSpelling,
+    Pronunciation,
+    DictionaryEntry,
+]
 
 # Main Site admin settings. For related sites models, see .sites_admin
 
@@ -43,3 +61,11 @@ admin.site.unregister(Group)
 admin.site.unregister(SocialAccount)
 admin.site.unregister(SocialApp)
 admin.site.unregister(SocialToken)
+
+# Dictionary models
+# todo: make suitable admin objects to register for each model
+# todo: make custom admin forms to prevent self selection in ManyToMany fields referring to self
+# ref: https://stackoverflow.com/questions/869856/
+admin.site.register(dictionary_models)
+admin.site.register(Category)
+admin.site.register(PartOfSpeech)
