e7ed2176f6b4f5fe432bc7f45ac2aa668082b9d3
Nolan Van Hell <nolan@fpcc.ca>
2023-03-27 09:44:51 -0700

    FW-4198 Add ViewSet mixin enabling list view permissions

--- fv_be/app/serializers.py
+++ fv_be/app/serializers.py
@@ -4,6 +4,8 @@ from .models.sites import Site
 
 
 class SiteSerializer(serializers.ModelSerializer):
+    visibility = serializers.CharField(source="get_visibility_display")
+
     class Meta:
         model = Site
         fields = ["id", "title", "slug", "language", "language_family", "visibility"]
