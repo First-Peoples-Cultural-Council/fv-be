6334576e437bb9a9578247a0de3f45fd95cd5e0a
Bronson Chapman <bronson@fpcc.ca>
2023-03-27 17:46:27 -0600

    fix uniqueness bugs, add related dictionary entry validation, add site to base character str

--- fv_be/app/admin/characters_admin.py
+++ fv_be/app/admin/characters_admin.py
@@ -1,12 +1,33 @@
 from django.contrib import admin
 
 from fv_be.app.models.characters import Character, CharacterVariant, IgnoredCharacter
+from fv_be.app.models.dictionary import DictionaryEntry
 
+# FirstVoices
 from .base_admin import BaseInlineAdmin, BaseSiteContentAdmin
 
 
-# TODO display title, site when selecting a base character
-# TODO Only show related dictionary entries for the same site
+class CharacterRelatedDictionaryEntryInline(BaseInlineAdmin):
+    model = Character.related_dictionary_entries.through
+    fields = ("character", "dictionary_entry")
+    readonly_fields = BaseInlineAdmin.readonly_fields
+    can_delete = True
+
+    # TODO: When adding a character, all related dictionary entries are shown. This should be filtered by site.
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "dictionary_entry":
+            # Get the Character from the request and filter dictionary entries by its site_id
+            character_id = request.resolver_match.kwargs.get("object_id")
+            if character_id:
+                queryset = DictionaryEntry.objects.filter(
+                    site_id=self.model.character.field.related_model.objects.get(
+                        pk=character_id
+                    ).site_id
+                )
+                kwargs["queryset"] = queryset
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
 @admin.register(Character)
 class CharacterAdmin(BaseSiteContentAdmin):
     filter_horizontal = ("related_dictionary_entries",)
@@ -16,7 +37,6 @@ class CharacterAdmin(BaseSiteContentAdmin):
         "approximate_form",
         "site",
         "notes",
-        "related_dictionary_entries",
     )
     list_display = (
         "title",
@@ -24,6 +44,7 @@ class CharacterAdmin(BaseSiteContentAdmin):
         "approximate_form",
     ) + BaseSiteContentAdmin.list_display
     search_fields = ("title", "approximate_form")
+    inlines = (CharacterRelatedDictionaryEntryInline,)
 
 
 @admin.register(CharacterVariant)
@@ -44,7 +65,6 @@ class IgnoredCharacterAdmin(BaseSiteContentAdmin):
     search_fields = ("title",)
 
 
-# TODO the horizontal filter is not the prettiest in an inline, consider changing in the future
 class CharacterInline(BaseInlineAdmin):
     filter_horizontal = ("related_dictionary_entries",)
     model = Character
@@ -53,9 +73,9 @@ class CharacterInline(BaseInlineAdmin):
         "sort_order",
         "approximate_form",
         "notes",
-        "related_dictionary_entries",
     ) + BaseInlineAdmin.fields
     readonly_fields = BaseInlineAdmin.readonly_fields + CharacterAdmin.readonly_fields
+    inlines = (CharacterRelatedDictionaryEntryInline,)
 
 
 class CharacterVariantInline(BaseInlineAdmin):
--- fv_be/app/models/characters.py
+++ fv_be/app/models/characters.py
@@ -17,12 +17,22 @@ class Character(BaseSiteContentModel):
     class Meta:
         verbose_name = _("character")
         verbose_name_plural = _("characters")
+        constraints = [
+            models.UniqueConstraint(
+                fields=["title", "site_id"], name="unique_character"
+            ),
+            models.UniqueConstraint(
+                fields=["sort_order", "site_id"], name="unique_character_sort_order"
+            ),
+        ]
 
     # from dc:title
-    title = models.CharField(max_length=MAX_CHARACTER_LENGTH, unique=True)
+    # Unique with site_id
+    title = models.CharField(max_length=MAX_CHARACTER_LENGTH)
 
     # from fvcharacter:alphabet_order
-    sort_order = models.IntegerField(unique=True)
+    # Unique with site_id
+    sort_order = models.IntegerField()
 
     # from fvcharacter:fuzzy_latin_match
     approximate_form = models.CharField(max_length=MAX_CHARACTER_LENGTH, blank=True)
@@ -34,12 +44,48 @@ class Character(BaseSiteContentModel):
     related_dictionary_entries = models.ManyToManyField(
         DictionaryEntry,
         blank=True,
-        null=True,
+        through="CharacterRelatedDictionaryEntry",
         related_name="characters",
     )
 
     def __str__(self):
-        return self.title
+        return f"{self.title} - {self.site}"
+
+
+class CharacterRelatedDictionaryEntry(BaseSiteContentModel):
+    """
+    Represents a link between a character and a dictionary entry.
+    """
+
+    class Meta:
+        verbose_name = _("character related dictionary entry")
+        verbose_name_plural = _("character related dictionary entries")
+
+    character = models.ForeignKey(
+        Character,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="dictionary_entry_links",
+    )
+
+    dictionary_entry = models.ForeignKey(
+        DictionaryEntry,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="character_links",
+    )
+
+    def __str__(self):
+        return f"{self.character} - {self.dictionary_entry}"
+
+    def save(self, *args, **kwargs):
+        self.set_site_id()
+        super().save(*args, **kwargs)
+
+    def set_site_id(self):
+        self.site = self.character.site
 
 
 class CharacterVariant(BaseSiteContentModel):
@@ -50,9 +96,15 @@ class CharacterVariant(BaseSiteContentModel):
     class Meta:
         verbose_name = _("character variant")
         verbose_name_plural = _("character variants")
+        constraints = [
+            models.UniqueConstraint(
+                fields=["title", "site_id"], name="unique_character_variant"
+            )
+        ]
 
     # from fvcharacter: upper_case_character
-    title = models.CharField(max_length=MAX_CHARACTER_LENGTH, unique=True)
+    # Unique with site_id
+    title = models.CharField(max_length=MAX_CHARACTER_LENGTH)
 
     base_character = models.ForeignKey(
         Character, on_delete=models.CASCADE, related_name="variants"
@@ -77,9 +129,15 @@ class IgnoredCharacter(BaseSiteContentModel):
     class Meta:
         verbose_name = _("ignored character")
         verbose_name_plural = _("ignored characters")
+        constraints = [
+            models.UniqueConstraint(
+                fields=["title", "site_id"], name="unique_ignored_character"
+            )
+        ]
 
     # from fv-alphabet:ignored_characters
-    title = models.CharField(max_length=MAX_CHARACTER_LENGTH, unique=True)
+    # Unique with site_id
+    title = models.CharField(max_length=MAX_CHARACTER_LENGTH)
 
     def __str__(self):
         return self.title
