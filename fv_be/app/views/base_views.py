30de72e43ec8aebef61fb8ec70f764448e09fa60
Nolan Van Hell <nolan@fpcc.ca>
2023-03-28 16:44:59 -0700

    FW-4198 Add ViewSet mixin enabling list view permissions (#20)
    * FW-4198 Add ViewSet mixin enabling list view permissions
    
    * FW-4198 Update list view permissions mixin name

---
+++ fv_be/app/views/base_views.py
@@ -0,0 +1,24 @@
+from rest_framework.response import Response
+from rules.contrib.rest_framework import AutoPermissionViewSetMixin
+
+
+class FVPermissionViewSetMixin(AutoPermissionViewSetMixin):
+    """
+    Mixin used to override the default list view and enable the use of rules based permissions as defined in a model.
+    If this mixin is used, the list view will return all model instances that the user has view permissions on.
+    """
+
+    def list(self, request, *args, **kwargs):
+        # Get the list of model UUIDs that the user has view permissions for
+        models_uuid_list = [
+            model.id
+            for model in self.get_queryset()
+            if self.request.user.has_perm(
+                f"app.view_{model.__class__.__name__.lower()}", model
+            )
+        ]
+        # Create a queryset filtered by the UUID list
+        queryset = self.queryset.model.objects.filter(id__in=models_uuid_list)
+        # Serialize and return the data
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
--- fv_be/app/views/sites_views.py
+++ fv_be/app/views/sites_views.py
@@ -1,22 +1,11 @@
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rules.contrib.rest_framework import AutoPermissionViewSetMixin
 
 from fv_be.app.serializers import Site, SiteSerializer
+from fv_be.app.views.base_views import FVPermissionViewSetMixin
 
 
-class SiteViewSet(AutoPermissionViewSetMixin, ModelViewSet):
+class SiteViewSet(FVPermissionViewSetMixin, ModelViewSet):
     # todo: real api implementation
     http_method_names = ["get"]
     serializer_class = SiteSerializer
     queryset = Site.objects.all()
-
-    def list(self, request, **kwargs):
-        sites_uuid_list = [
-            site.id
-            for site in self.get_queryset()
-            if self.request.user.has_perm("app.view_site", site)
-        ]
-        queryset = Site.objects.filter(id__in=sites_uuid_list)
-        serializer = SiteSerializer(queryset, many=True)
-        return Response(serializer.data)
