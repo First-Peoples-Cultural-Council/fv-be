6fa5dfb4b67e869b0aa9625121b82ca7be3d2767
Robert Johnstone <rob@plasticviking.com>
2023-03-28 18:35:25 +0800

    allow unauthenticated health check

--- firstvoices/backend/urls.py
+++ firstvoices/backend/urls.py
@@ -1,9 +1,14 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
+from backend.views import HealthCheckView
 from backend.viewsets.User import UserViewSet
 
 ROUTER = DefaultRouter(trailing_slash=False)
 ROUTER.register(r'api/1.0/user', UserViewSet, basename=r'user')
 
-urlpatterns = []
+urlpatterns = [
+	path(r'health', HealthCheckView.as_view())
+]
+
 urlpatterns += ROUTER.urls
--- firstvoices/backend/views.py
+++ firstvoices/backend/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class HealthCheckView(APIView):
+	permission_classes = [permissions.AllowAny]
+	authentication_classes = []
+
+	def get(self, request, format=None):
+		return Response({"status": "server is ok"})
+
