ff29e5275e40ef3a6bd2914bc1dafc3dfef99095
Sahil Sharma <sahil.sharma071997@gmail.com>
2023-04-04 14:18:40 -0700

    Added description to PartsOfSpeech viewset for API documentation

--- src/firstvoices/backend/views/dictionary_views.py
+++ src/firstvoices/backend/views/dictionary_views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework.viewsets import ModelViewSet
 
 from firstvoices.backend.serializers.dictionary_serializers import (
@@ -7,6 +8,13 @@ from firstvoices.backend.serializers.dictionary_serializers import (
 from firstvoices.backend.views.base_views import FVPermissionViewSetMixin
 
 
+@extend_schema_view(
+    list=extend_schema(
+        description="A list of different parts-of-speech available that different words/phrases can be assigned to.",
+        responses={200: PartsOfSpeechSerializer},
+    ),
+    retrieve=extend_schema(description="Details about a specific parts-of-speech."),
+)
 class PartsOfSpeechViewSet(FVPermissionViewSetMixin, ModelViewSet):
     http_method_names = ["get"]
     serializer_class = PartsOfSpeechSerializer
