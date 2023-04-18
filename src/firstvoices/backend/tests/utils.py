c2478638f0c77c442c0bfbf0712ec3d9e5a72a97
Sahil Sharma <sahil.sharma071997@gmail.com>
2023-04-18 14:47:31 -0700

    Added NOSONAR to ignore the sonar warning for using random in a test file

--- src/firstvoices/backend/tests/utils.py
+++ src/firstvoices/backend/tests/utils.py
@@ -5,4 +5,4 @@ import string
 def generate_string(length):
     """Function to generate string of ascii characters (both upper and lower case) for the given length."""
     letters = string.ascii_letters
-    return "".join(random.choice(letters) for i in range(length))
+    return "".join(random.choice(letters) for _ in range(length))  # NOSONAR
