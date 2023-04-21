7b89e1054b20bd51fdeee041d72fbcb43e9270a3
Bronson Chapman <bronsonchapman01@gmail.com>
2023-04-21 11:48:18 -0600

    remove unneeded serializers

--- src/firstvoices/backend/serializers/character_serializers.py
+++ src/firstvoices/backend/serializers/character_serializers.py
@@ -1,10 +1,6 @@
 from rest_framework import serializers
 
-from firstvoices.backend.models.characters import (
-    Character,
-    CharacterVariant,
-    IgnoredCharacter,
-)
+from firstvoices.backend.models.characters import Character
 
 
 class CharacterSerializer(serializers.ModelSerializer):
@@ -13,20 +9,3 @@ class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
         fields = ["id", "title", "sort_order", "approximate_form", "notes", "site"]
-
-
-class CharacterVariantSerializer(serializers.ModelSerializer):
-    site = serializers.StringRelatedField()
-    base_character = serializers.StringRelatedField()
-
-    class Meta:
-        model = CharacterVariant
-        fields = ["id", "title", "base_character", "site"]
-
-
-class IgnoredCharacterSerializer(serializers.ModelSerializer):
-    site = serializers.StringRelatedField()
-
-    class Meta:
-        model = IgnoredCharacter
-        fields = ["id", "title", "site"]
