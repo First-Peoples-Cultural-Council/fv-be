6e0112e5a079e65c126bb09a0326d5da4f51e2ae
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2023-05-04 12:13:37 -0700

    fw-4255, predicate tests passing

--- firstvoices/backend/permissions/filters/view_models.py
+++ firstvoices/backend/permissions/filters/view_models.py
@@ -8,8 +8,8 @@ from . import base
 # Site model is visible to all unless it has Team-only visibility
 def can_view_site_model(user):
     return (
-        base.is_public_obj(user)
-        | base.is_members_obj(user)
+        base.is_public_obj()
+        | base.is_members_obj()
         | base.is_at_least_staff_admin(user)
         | base.has_team_access_to_obj(user)
     )
