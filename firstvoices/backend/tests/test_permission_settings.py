e02d25d53b0d3f6da9ffff9002ea5247606c9ec2
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2023-05-04 15:37:47 -0700

    fw-4255, all tests passing!
    * skipping tests for User model, since it isn't fully implemented yet
    * tests passing for dictionary sub-models

--- firstvoices/backend/tests/test_permission_settings.py
+++ firstvoices/backend/tests/test_permission_settings.py
@@ -3,7 +3,10 @@ import pytest
 from django.apps import apps
 from factory.django import DjangoModelFactory
 
+from backend.models import User
+from backend.models.base import BaseControlledSiteContentModel, BaseSiteContentModel
 from backend.models.constants import AppRole, Role, Visibility
+from backend.models.dictionary import BaseDictionaryContentModel
 from backend.tests import factories
 
 
@@ -22,7 +25,9 @@ def get_permitted_ids(user, queryset):
 
 
 class TestPermissionManager:
-    models = apps.get_app_config("backend").get_models()
+    # test all models
+    # ... but skip User until fw-4165
+    models = [m for m in apps.get_app_config("backend").get_models() if not m == User]
 
     @pytest.mark.parametrize(
         "model_cls",
@@ -64,17 +69,7 @@ class TestPermissionManager:
     def assert_visible_matches(self, model_cls, user, user_role):
         model_factory = self.get_or_create_factory(model_cls)
 
-        # generate test data with the necessary attributes
-        if hasattr(model_cls, "site"):
-            if hasattr(model_cls, "visibility"):
-                self.generate_sites_with_controlled_test_data(
-                    model_factory, user, user_role
-                )
-            else:
-                self.generate_sites_with_test_data(model_factory, user, user_role)
-
-        else:
-            model_factory.create()
+        self.generate_test_data(model_cls, model_factory, user, user_role)
 
         visible_by_permission_rules = get_permitted_ids(user, model_cls.objects.all())
         visible_by_permission_manager = {
@@ -89,7 +84,30 @@ class TestPermissionManager:
         }
         assert visible_by_permission_filter == visible_by_permission_rules
 
-    def generate_sites_with_test_data(self, model_factory, user, user_role):
+    def generate_test_data(self, model_cls, model_factory, user, user_role):
+        """
+        Attempt to automatically generate the right kind of data. This will sometimes need to be updated
+        when we add new kinds of models.
+        """
+
+        if issubclass(model_cls, BaseDictionaryContentModel):
+            self.generate_dictionary_content_test_data(model_factory, user, user_role)
+            return
+
+        if issubclass(model_cls, BaseControlledSiteContentModel):
+            self.generate_controlled_site_content_test_data(
+                model_factory, user, user_role
+            )
+            return
+
+        if issubclass(model_cls, BaseSiteContentModel):
+            self.generate_site_content_test_data(model_factory, user, user_role)
+            return
+
+        # models that use BaseModel
+        model_factory.create()
+
+    def generate_site_content_test_data(self, model_factory, user, user_role):
         for site_visibility in Visibility:
             # create a site with a membership
             site1 = factories.SiteFactory.create(visibility=site_visibility)
@@ -104,7 +122,9 @@ class TestPermissionManager:
             # create object in the site
             model_factory.create(site=site2)
 
-    def generate_sites_with_controlled_test_data(self, model_factory, user, user_role):
+    def generate_controlled_site_content_test_data(
+        self, model_factory, user, user_role
+    ):
         for site_visibility in Visibility:
             # create a site with a membership
             site1 = factories.SiteFactory.create(visibility=site_visibility)
@@ -120,3 +140,26 @@ class TestPermissionManager:
             # create objects in the site
             for v2 in Visibility:
                 model_factory.create(visibility=v2, site=site2)
+
+    def generate_dictionary_content_test_data(self, model_factory, user, user_role):
+        for site_visibility in Visibility:
+            # create a site with a membership
+            site1 = factories.SiteFactory.create(visibility=site_visibility)
+            factories.MembershipFactory(site=site1, user=user, role=user_role)
+
+            # create objects in the site
+            for v1 in Visibility:
+                entry1 = factories.DictionaryEntryFactory.create(
+                    site=site1, visibility=v1
+                )
+                model_factory.create(dictionary_entry=entry1)
+
+            # create a site with no membership
+            site2 = factories.SiteFactory.create(visibility=site_visibility)
+
+            # create objects in the site
+            for v2 in Visibility:
+                entry2 = factories.DictionaryEntryFactory.create(
+                    site=site2, visibility=v2
+                )
+                model_factory.create(dictionary_entry=entry2)
