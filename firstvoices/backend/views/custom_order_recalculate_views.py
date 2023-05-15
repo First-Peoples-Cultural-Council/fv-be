754c7f28c518a1c0a08174fad629f4410ee732e1
Bronson Chapman <bronsonchapman01@gmail.com>
2023-05-15 15:46:27 -0600

    add results to recalculation status

--- firstvoices/backend/views/custom_order_recalculate_views.py
+++ firstvoices/backend/views/custom_order_recalculate_views.py
@@ -9,27 +9,44 @@ from backend.tasks.alphabet_tasks import recalculate_custom_order_preview
 
 class CustomOrderRecalculatePreviewView(APIView):
     permission_classes = [permissions.IsAdminUser]
+    task_id = None
 
-    @staticmethod
-    def get(request, site_slug: str):
+    def get(self, request, site_slug: str):
         result = (
             CustomOrderRecalculationPreviewResult.objects.filter(site__slug=site_slug)
             .order_by("-date")
             .first()
         )
-        if result:
-            return Response({"recalculation_results": result.result}, status=200)
-        else:
+
+        # If there is no result and no task_id, then there are no recalculation results for this site
+        if not self.task_id and not result:
             return Response(
                 {"message": "No recalculation results found for this site."}, status=404
             )
 
-    @staticmethod
-    def post(request, site_slug: str):
+        # If there is a task_id, check the status of the task and add it to the response
+        if self.task_id:
+            async_result = AsyncResult(self.task_id)
+            preview_info = {"recalculate_preview_task_status": async_result.status}
+        else:
+            preview_info = {"recalculate_preview_task_status": "Not started."}
+
+        # If there is a result, add it to the response
+        if result:
+            preview_info["last_recalculation_date"] = result.date
+            preview_info["last_recalculation_result"] = result.result
+        else:
+            preview_info["last_recalculation_date"] = None
+            preview_info["last_recalculation_result"] = {}
+
+        return Response(preview_info, status=200)
+
+    def post(self, request, site_slug: str):
         try:
-            recalculation_results: AsyncResult = (
-                recalculate_custom_order_preview.apply_async((site_slug,))
+            recalculation_results = recalculate_custom_order_preview.apply_async(
+                (site_slug,)
             )
+            self.task_id = recalculation_results.task_id
 
             site = Site.objects.get(slug=site_slug)
             CustomOrderRecalculationPreviewResult.objects.create(
