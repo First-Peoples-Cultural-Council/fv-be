432375b94e05360eff14863546a7de7ffded1d59
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2023-05-18 14:31:40 -0700

    fw-4387, enhanced stub model for images
    - organizes files into site folders
    - deletes associated file when Image instance is deleted

--- firstvoices/backend/models/media.py
+++ firstvoices/backend/models/media.py
@@ -3,6 +3,11 @@ from django.db import models
 from .base import BaseSiteContentModel
 
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/<site slug>/<filename>
+    return f"{instance.site.slug}/{filename}"
+
+
 class Image(BaseSiteContentModel):
     """
     Stub
@@ -13,5 +18,21 @@ class Image(BaseSiteContentModel):
     # from dc:title
     title = models.CharField(max_length=200)
 
+    # organizes file uploads into site folders. Could also add a subfolder for each media type (/site/images/file.jpg)
     # from fvm:content, see fw-4352 for migration details
-    content = models.ImageField()
+    content = models.ImageField(upload_to=user_directory_path)
+
+    def delete(self, using=None, keep_parents=False):
+        """
+        Deletes the associated media file when the instance is deleted, to prevent orphans.
+        """
+        result = super().delete(using, keep_parents)
+        try:
+            self.content.delete(save=False)
+        except Exception as e:
+            # this will only happen for connection or permission errors, so it's a warning
+            self.logger.warn(
+                f"Failed to delete file from S3 when deleting [{str(self)}]. Error: {e} "
+            )
+
+        return result
