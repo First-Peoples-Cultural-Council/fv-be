f2e0add6a5210d3db9489c20d73591f0fb24d697
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2023-05-24 16:52:16 -0700

    remove unused code

--- firstvoices/backend/tests/factories/dictionary_entry.py
+++ firstvoices/backend/tests/factories/dictionary_entry.py
@@ -12,14 +12,3 @@ class DictionaryEntryFactory(RelatedMediaBaseFactory):
     site = factory.SubFactory(SiteFactory)
     title = factory.Sequence(lambda n: "Dictionary entry %03d" % n)
     custom_order = factory.Sequence(lambda n: "sort order %03d" % n)
-
-    @factory.post_generation
-    def related_images(self, create, extracted, **kwargs):
-        if not create:
-            # Simple build, do nothing.
-            return
-
-        if extracted:
-            # A list of image were passed in, use them
-            for image in extracted:
-                self.related_images.add(image)
