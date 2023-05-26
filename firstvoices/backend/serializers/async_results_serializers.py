aa9e4680c9482779b9d105720fdc885e3c3230fe
Bronson Chapman <bronsonchapman01@gmail.com>
2023-05-26 12:46:13 -0600

    fix json ordering

--- firstvoices/backend/serializers/async_results_serializers.py
+++ firstvoices/backend/serializers/async_results_serializers.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from celery.result import AsyncResult
 from rest_framework import serializers
 
@@ -9,10 +11,35 @@ class CustomOrderRecalculationPreviewResultDetailSerializer(
 ):
     site = serializers.StringRelatedField()
     current_preview_task_status = serializers.SerializerMethodField()
+    latest_recalculation_result = serializers.SerializerMethodField()
 
     def get_current_preview_task_status(self, obj):
         async_result = AsyncResult(obj.task_id)
         return async_result.status
+
+    def get_latest_recalculation_result(self, obj):
+        ordered_result = OrderedDict(
+            {
+                "unknown_character_count": obj.latest_recalculation_result[
+                    "unknown_character_count"
+                ],
+                "updated_entries": [],
+            }
+        )
+
+        for entry in obj.latest_recalculation_result["updated_entries"]:
+            ordered_entry = OrderedDict(
+                {
+                    "title": entry["title"],
+                    "cleaned_title": entry["cleaned_title"],
+                    "is_title_updated": entry["is_title_updated"],
+                    "previous_custom_order": entry["previous_custom_order"],
+                    "new_custom_order": entry["new_custom_order"],
+                }
+            )
+            ordered_result["updated_entries"].append(ordered_entry)
+
+        return ordered_result
 
     class Meta:
         model = CustomOrderRecalculationPreviewResult
