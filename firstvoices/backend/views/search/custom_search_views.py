5e5cbdf0353da704836bb5ac67ecdc6a84752c7b
Sahil Sharma <sahil.sharma071997@gmail.com>
2023-05-29 13:45:56 -0700

    Added custom exception class for elastic-search connection error

--- firstvoices/backend/views/search/custom_search_views.py
+++ firstvoices/backend/views/search/custom_search_views.py
@@ -1,8 +1,10 @@
+from elasticsearch.exceptions import ConnectionError
 from rest_framework import mixins, viewsets
 from rest_framework.response import Response
 
 from backend.search.query_builder import get_search_query
 from backend.search.utils.object_utils import hydrate_objects
+from backend.views.exceptions import ElasticSearchConnectionError
 
 
 class CustomSearchViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -26,7 +28,11 @@ class CustomSearchViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
         search_query = get_search_query(q=search_params["q"])
 
         # Get search results
-        response = search_query.execute()
+        try:
+            response = search_query.execute()
+        except ConnectionError:
+            raise ElasticSearchConnectionError()
+
         search_results = response["hits"]["hits"]
 
         # Adding data to objects
