dd8b609b9794e506d3f91cfc42f38bcc6a8b6963
Bronson Chapman <bronsonchapman01@gmail.com>
2023-06-01 16:41:03 -0600

    parameterize unit tests

--- firstvoices/backend/tests/test_apis/test_dictionary_cleanup_api.py
+++ firstvoices/backend/tests/test_apis/test_dictionary_cleanup_api.py
@@ -8,13 +8,14 @@ from backend.models.constants import AppRole, Visibility
 from backend.tests import factories
 from backend.tests.test_apis.base_api_test import BaseApiTest
 
+RECALCULATION_CONFIRMATION_MESSAGE = {"message": "Recalculation has been queued."}
+RECALCULATION_PREVIEW_CONFIRMATION_MESSAGE = {
+    "message": "Recalculation preview has been queued."
+}
+
 
 class TestDictionaryCleanup(BaseApiTest):
     API_DETAIL_VIEW = "api:site-detail"
-    RECALCULATION_CONFIRMATION_MESSAGE = {"message": "Recalculation has been queued."}
-    RECALCULATION_PREVIEW_CONFIRMATION_MESSAGE = {
-        "message": "Recalculation preview has been queued."
-    }
 
     def get_cleanup_endpoint(self, key, is_preview):
         if is_preview:
@@ -23,27 +24,30 @@ class TestDictionaryCleanup(BaseApiTest):
             return self.get_detail_endpoint(key) + "dictionary-cleanup/"
 
     @pytest.mark.django_db
-    def test_recalculate_preview_get_404(self):
+    @pytest.mark.parametrize("is_preview", [True, False])
+    def test_recalculate_get_404(self, is_preview):
         user = factories.get_app_admin(role=AppRole.SUPERADMIN)
         self.client.force_authenticate(user=user)
 
         response = self.client.get(
-            self.get_cleanup_endpoint("missing-slug", is_preview=True)
+            self.get_cleanup_endpoint("missing-slug", is_preview=is_preview)
         )
         assert response.status_code == 404
 
     @pytest.mark.django_db
-    def test_recalculate_preview_post_404(self):
+    @pytest.mark.parametrize("is_preview", [True, False])
+    def test_recalculate_post_404(self, is_preview):
         user = factories.get_app_admin(role=AppRole.SUPERADMIN)
         self.client.force_authenticate(user=user)
 
         response = self.client.post(
-            self.get_cleanup_endpoint("missing-slug", is_preview=True)
+            self.get_cleanup_endpoint("missing-slug", is_preview=is_preview)
         )
         assert response.status_code == 404
 
     @pytest.mark.django_db
-    def test_recalculate_preview_post_403(self):
+    @pytest.mark.parametrize("is_preview", [True, False])
+    def test_recalculate_post_403(self, is_preview):
         site = factories.SiteFactory.create(slug="test", visibility=Visibility.PUBLIC)
         factories.AlphabetFactory.create(site=site)
 
@@ -51,12 +55,13 @@ class TestDictionaryCleanup(BaseApiTest):
         self.client.force_authenticate(user=user)
 
         response = self.client.post(
-            self.get_cleanup_endpoint(site.slug, is_preview=True)
+            self.get_cleanup_endpoint(site.slug, is_preview=is_preview)
         )
         assert response.status_code == 403
 
     @pytest.mark.django_db
-    def test_recalculate_preview_get_empty(self):
+    @pytest.mark.parametrize("is_preview", [True, False])
+    def test_recalculate_get_empty(self, is_preview):
         site = factories.SiteFactory.create(slug="test", visibility=Visibility.PUBLIC)
         factories.AlphabetFactory.create(site=site)
 
@@ -71,30 +76,20 @@ class TestDictionaryCleanup(BaseApiTest):
         assert response_data["results"] == []
 
     @pytest.mark.django_db(transaction=True, serialized_rollback=True)
-    def test_recalculate_preview_post(
-        self, celery_worker, django_db_serialized_rollback
-    ):
-        site = factories.SiteFactory.create(slug="test", visibility=Visibility.PUBLIC)
-        factories.AlphabetFactory.create(site=site)
-
-        user = factories.get_app_admin(role=AppRole.SUPERADMIN)
-        self.client.force_authenticate(user=user)
-
-        factories.DictionaryEntryFactory.create(site=site, title="test")
-
-        response = self.client.post(
-            self.get_cleanup_endpoint(site.slug, is_preview=True)
-        )
-
-        response_data = json.loads(response.content)
-        assert response.status_code == 201
-        assert response_data == self.RECALCULATION_PREVIEW_CONFIRMATION_MESSAGE
-
-    @pytest.mark.django_db(transaction=True, serialized_rollback=True)
-    def test_recalculate_preview_full(
+    @pytest.mark.parametrize(
+        "is_preview, result",
+        [
+            (True, RECALCULATION_PREVIEW_CONFIRMATION_MESSAGE),
+            (False, RECALCULATION_CONFIRMATION_MESSAGE),
+        ],
+    )
+    def test_recalculate_post(
         self,
-        celery_worker,
+        celery_session_worker,
+        celery_session_app,
         django_db_serialized_rollback,
+        is_preview,
+        result,
     ):
         site = factories.SiteFactory.create(slug="test", visibility=Visibility.PUBLIC)
         factories.AlphabetFactory.create(site=site)
@@ -104,134 +99,30 @@ class TestDictionaryCleanup(BaseApiTest):
 
         factories.DictionaryEntryFactory.create(site=site, title="test")
 
-        response_post = self.client.post(
-            self.get_cleanup_endpoint(site.slug, is_preview=True)
-        )
-        response_post_data = json.loads(response_post.content)
-
-        assert response_post.status_code == 201
-        assert response_post_data == self.RECALCULATION_PREVIEW_CONFIRMATION_MESSAGE
-        response_get = self.client.get(
-            self.get_cleanup_endpoint(site.slug, is_preview=True)
-        )
-        response_get_data = json.loads(response_get.content)
-
-        assert response_get.status_code == 200
-        assert response_get_data["results"] == [
-            {
-                "site": site.title,
-                "currentTaskStatus": "SUCCESS",
-                "latestRecalculationDate": ANY,
-                "latestRecalculationResult": {
-                    "updatedEntries": [],
-                    "unknownCharacterCount": {"⚑e": 1, "⚑s": 1, "⚑t": 2},
-                },
-            }
-        ]
-
-    @pytest.mark.django_db(transaction=True, serialized_rollback=True)
-    def test_recalculate_preview_permissions(
-        self, celery_worker, django_db_serialized_rollback
-    ):
-        site = factories.SiteFactory.create(slug="test", visibility=Visibility.PUBLIC)
-        factories.AlphabetFactory.create(site=site)
-
-        user = factories.get_app_admin(role=AppRole.SUPERADMIN)
-        self.client.force_authenticate(user=user)
-
-        factories.DictionaryEntryFactory.create(site=site, title="test")
-
-        response_post = self.client.post(
-            self.get_cleanup_endpoint(site.slug, is_preview=True)
-        )
-        response_post_data = json.loads(response_post.content)
-
-        assert response_post.status_code == 201
-        assert response_post_data == self.RECALCULATION_PREVIEW_CONFIRMATION_MESSAGE
-
-        user = factories.get_non_member_user()
-        self.client.force_authenticate(user=user)
-
-        response_get = self.client.get(
-            self.get_cleanup_endpoint(site.slug, is_preview=True)
-        )
-        response_get_data = json.loads(response_get.content)
-
-        assert response_get.status_code == 200
-        assert response_get_data["results"] == []
-
-    @pytest.mark.django_db
-    def test_recalculate_get_404(self):
-        user = factories.get_app_admin(role=AppRole.SUPERADMIN)
-        self.client.force_authenticate(user=user)
-
-        response = self.client.get(
-            self.get_cleanup_endpoint("missing-slug", is_preview=False)
-        )
-        assert response.status_code == 404
-
-    @pytest.mark.django_db
-    def test_recalculate_post_404(self):
-        user = factories.get_app_admin(role=AppRole.SUPERADMIN)
-        self.client.force_authenticate(user=user)
-
         response = self.client.post(
-            self.get_cleanup_endpoint("missing-slug", is_preview=False)
-        )
-        assert response.status_code == 404
-
-    @pytest.mark.django_db
-    def test_recalculate_post_403(self):
-        site = factories.SiteFactory.create(slug="test", visibility=Visibility.PUBLIC)
-        factories.AlphabetFactory.create(site=site)
-
-        user = factories.get_non_member_user()
-        self.client.force_authenticate(user=user)
-
-        response = self.client.post(
-            self.get_cleanup_endpoint(site.slug, is_preview=False)
-        )
-        assert response.status_code == 403
-
-    @pytest.mark.django_db
-    def test_recalculate_get_empty(self):
-        site = factories.SiteFactory.create(slug="test", visibility=Visibility.PUBLIC)
-        factories.AlphabetFactory.create(site=site)
-
-        user = factories.get_app_admin(role=AppRole.SUPERADMIN)
-        self.client.force_authenticate(user=user)
-
-        response = self.client.get(
-            self.get_cleanup_endpoint(site.slug, is_preview=False)
-        )
-
-        response_data = json.loads(response.content)
-        assert response.status_code == 200
-        assert response_data["results"] == []
-
-    @pytest.mark.django_db(transaction=True, serialized_rollback=True)
-    def test_recalculate_post(self, celery_worker, django_db_serialized_rollback):
-        site = factories.SiteFactory.create(slug="test", visibility=Visibility.PUBLIC)
-        factories.AlphabetFactory.create(site=site)
-
-        user = factories.get_app_admin(role=AppRole.SUPERADMIN)
-        self.client.force_authenticate(user=user)
-
-        factories.DictionaryEntryFactory.create(site=site, title="test")
-        factories.CharacterFactory.create(site=site, title="t")
-        factories.CharacterFactory.create(site=site, title="e")
-        factories.CharacterFactory.create(site=site, title="s")
-
-        response = self.client.post(
-            self.get_cleanup_endpoint(site.slug, is_preview=False)
+            self.get_cleanup_endpoint(site.slug, is_preview=is_preview)
         )
 
         response_data = json.loads(response.content)
         assert response.status_code == 201
-        assert response_data == self.RECALCULATION_CONFIRMATION_MESSAGE
+        assert response_data == result
 
     @pytest.mark.django_db(transaction=True, serialized_rollback=True)
-    def test_recalculate_e2e(self, celery_worker, django_db_serialized_rollback):
+    @pytest.mark.parametrize(
+        "is_preview, message",
+        [
+            (True, RECALCULATION_PREVIEW_CONFIRMATION_MESSAGE),
+            (False, RECALCULATION_CONFIRMATION_MESSAGE),
+        ],
+    )
+    def test_recalculate_e2e(
+        self,
+        celery_session_worker,
+        celery_session_app,
+        django_db_serialized_rollback,
+        is_preview,
+        message,
+    ):
         site = factories.SiteFactory.create(slug="test", visibility=Visibility.PUBLIC)
         alphabet = factories.AlphabetFactory.create(site=site)
 
@@ -246,15 +137,15 @@ class TestDictionaryCleanup(BaseApiTest):
         alphabet.save()
 
         response_post = self.client.post(
-            self.get_cleanup_endpoint(site.slug, is_preview=False)
+            self.get_cleanup_endpoint(site.slug, is_preview=is_preview)
         )
 
         response_post_data = json.loads(response_post.content)
         assert response_post.status_code == 201
-        assert response_post_data == self.RECALCULATION_CONFIRMATION_MESSAGE
+        assert response_post_data == message
 
         response_get = self.client.get(
-            self.get_cleanup_endpoint(site.slug, is_preview=False)
+            self.get_cleanup_endpoint(site.slug, is_preview=is_preview)
         )
 
         response_get_data = json.loads(response_get.content)
@@ -278,12 +169,26 @@ class TestDictionaryCleanup(BaseApiTest):
                 },
             }
         ]
-        assert DictionaryEntry.objects.get(id=entry.id).title == "test"
-        assert DictionaryEntry.objects.get(id=entry.id).custom_order == "!#$!"
+
+        if not is_preview:
+            assert DictionaryEntry.objects.get(id=entry.id).title == "test"
+            assert DictionaryEntry.objects.get(id=entry.id).custom_order == "!#$!"
 
     @pytest.mark.django_db(transaction=True, serialized_rollback=True)
+    @pytest.mark.parametrize(
+        "is_preview, message",
+        [
+            (True, RECALCULATION_PREVIEW_CONFIRMATION_MESSAGE),
+            (False, RECALCULATION_CONFIRMATION_MESSAGE),
+        ],
+    )
     def test_recalculate_e2e_multiple_entries(
-        self, celery_worker, django_db_serialized_rollback
+        self,
+        celery_session_worker,
+        celery_session_app,
+        django_db_serialized_rollback,
+        is_preview,
+        message,
     ):
         site = factories.SiteFactory.create(slug="test", visibility=Visibility.PUBLIC)
         alphabet = factories.AlphabetFactory.create(site=site)
@@ -304,15 +209,15 @@ class TestDictionaryCleanup(BaseApiTest):
         alphabet.save()
 
         response_post = self.client.post(
-            self.get_cleanup_endpoint(site.slug, is_preview=False)
+            self.get_cleanup_endpoint(site.slug, is_preview=is_preview)
         )
 
         response_post_data = json.loads(response_post.content)
         assert response_post.status_code == 201
-        assert response_post_data == self.RECALCULATION_CONFIRMATION_MESSAGE
+        assert response_post_data == message
 
         response_get = self.client.get(
-            self.get_cleanup_endpoint(site.slug, is_preview=False)
+            self.get_cleanup_endpoint(site.slug, is_preview=is_preview)
         )
 
         response_get_data = json.loads(response_get.content)
@@ -343,14 +248,28 @@ class TestDictionaryCleanup(BaseApiTest):
                 },
             }
         ]
-        assert DictionaryEntry.objects.get(id=entry.id).title == "test"
-        assert DictionaryEntry.objects.get(id=entry.id).custom_order == "!#$!"
-        assert DictionaryEntry.objects.get(id=entry2.id).title == "AAA"
-        assert DictionaryEntry.objects.get(id=entry2.id).custom_order == "%%%"
+
+        if not is_preview:
+            assert DictionaryEntry.objects.get(id=entry.id).title == "test"
+            assert DictionaryEntry.objects.get(id=entry.id).custom_order == "!#$!"
+            assert DictionaryEntry.objects.get(id=entry2.id).title == "AAA"
+            assert DictionaryEntry.objects.get(id=entry2.id).custom_order == "%%%"
 
     @pytest.mark.django_db(transaction=True, serialized_rollback=True)
+    @pytest.mark.parametrize(
+        "is_preview, message",
+        [
+            (True, RECALCULATION_PREVIEW_CONFIRMATION_MESSAGE),
+            (False, RECALCULATION_CONFIRMATION_MESSAGE),
+        ],
+    )
     def test_recalculate_permissions(
-        self, celery_worker, django_db_serialized_rollback
+        self,
+        celery_session_worker,
+        celery_session_app,
+        django_db_serialized_rollback,
+        is_preview,
+        message,
     ):
         site = factories.SiteFactory.create(slug="test", visibility=Visibility.PUBLIC)
         factories.AlphabetFactory.create(site=site)
@@ -359,19 +278,22 @@ class TestDictionaryCleanup(BaseApiTest):
         self.client.force_authenticate(user=user)
 
         factories.DictionaryEntryFactory.create(site=site, title="test")
-        factories.CharacterFactory.create(site=site, title="t")
 
-        response = self.client.post(
-            self.get_cleanup_endpoint(site.slug, is_preview=False)
+        response_post = self.client.post(
+            self.get_cleanup_endpoint(site.slug, is_preview=is_preview)
         )
-        assert response.status_code == 201
+        response_post_data = json.loads(response_post.content)
+
+        assert response_post.status_code == 201
+        assert response_post_data == message
 
         user = factories.get_non_member_user()
         self.client.force_authenticate(user=user)
-        response = self.client.get(
-            self.get_cleanup_endpoint(site.slug, is_preview=False)
-        )
-        response_data = json.loads(response.content)
 
-        assert response.status_code == 200
-        assert response_data["results"] == []
+        response_get = self.client.get(
+            self.get_cleanup_endpoint(site.slug, is_preview=is_preview)
+        )
+        response_get_data = json.loads(response_get.content)
+
+        assert response_get.status_code == 200
+        assert response_get_data["results"] == []
