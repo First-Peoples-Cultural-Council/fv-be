7c5926858481d667fe8283e8a436cbfff3af1041
Sahil Sharma <sahil.sharma071997@gmail.com>
2023-06-01 14:03:26 -0700

    Added documentation for search API

--- firstvoices/backend/views/search/custom_search_views.py
+++ firstvoices/backend/views/search/custom_search_views.py
@@ -1,5 +1,13 @@
+from drf_spectacular.utils import (
+    OpenApiExample,
+    OpenApiParameter,
+    OpenApiResponse,
+    extend_schema,
+    extend_schema_view,
+    inline_serializer,
+)
 from elasticsearch.exceptions import ConnectionError
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, serializers, viewsets
 from rest_framework.response import Response
 
 from backend.search.query_builder import get_search_query
@@ -7,6 +15,35 @@ from backend.search.utils.object_utils import hydrate_objects
 from backend.views.exceptions import ElasticSearchConnectionError
 
 
+@extend_schema_view(
+    list=extend_schema(
+        description="List of search results satisfying the query.",
+        responses={
+            200: inline_serializer(
+                name="SearchResults",
+                fields={
+                    "id": serializers.CharField(),
+                    "score": serializers.FloatField(),
+                    "type": serializers.CharField(),
+                    "entry": serializers.DictField(),
+                },
+            ),
+            403: OpenApiResponse(description="Todo: Not authorized"),
+        },
+        parameters=[
+            OpenApiParameter(
+                name="q",
+                description="search term",
+                required=False,
+                type=str,
+                examples=[
+                    OpenApiExample("ball", value="ball"),
+                    OpenApiExample("quick brown fox", value="quick brown fox"),
+                ],
+            )
+        ],
+    ),
+)
 class CustomSearchViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     http_method_names = ["get"]
     queryset = ""
