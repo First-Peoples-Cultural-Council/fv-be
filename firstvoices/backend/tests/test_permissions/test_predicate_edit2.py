9ee8d3c6cac7b03782843c29f8d643a81532a17c
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2023-06-06 08:56:49 -0700

    test fix

--- firstvoices/backend/tests/test_permissions/test_predicate_edit2.py
+++ firstvoices/backend/tests/test_permissions/test_predicate_edit2.py
@@ -71,6 +71,10 @@ class TestCanEditCoreUncontrolledData:
 
 
 class TestCanAddCoreUncontrolledData:
+    """
+    These are tested with site objects because the "create" permission is tested with a site object.
+    """
+
     @pytest.mark.parametrize(
         "get_user", [get_anonymous_user, get_non_member_user, get_member_of_other_site]
     )
@@ -78,38 +82,32 @@ class TestCanAddCoreUncontrolledData:
     def test_non_members_blocked(self, get_user):
         user = get_user()
         site = SiteFactory.create(visibility=Visibility.PUBLIC)
-        obj = UncontrolledSiteContentFactory.build(site=site)
-        assert not edit2.can_add_core_uncontrolled_data(user, obj)
+        assert not edit2.can_add_core_uncontrolled_data(user, site)
 
     @pytest.mark.django_db
     def test_members_blocked(self):
         site, user = get_site_with_member(Visibility.PUBLIC, Role.MEMBER)
-        obj = UncontrolledSiteContentFactory.build(site=site)
-        assert not edit2.can_add_core_uncontrolled_data(user, obj)
+        assert not edit2.can_add_core_uncontrolled_data(user, site)
 
     @pytest.mark.django_db
     def test_staff_admin_blocked(self):
         user = get_app_admin(AppRole.STAFF)
         site = SiteFactory.create(visibility=Visibility.PUBLIC)
-        obj = UncontrolledSiteContentFactory.build(site=site)
-        assert not edit2.can_add_core_uncontrolled_data(user, obj)
+        assert not edit2.can_add_core_uncontrolled_data(user, site)
 
     @pytest.mark.parametrize("role", [Role.EDITOR, Role.LANGUAGE_ADMIN])
     @pytest.mark.django_db
     def test_editors_and_up_permitted(self, role):
         site, user = get_site_with_member(Visibility.PUBLIC, role)
-        obj = UncontrolledSiteContentFactory.build(site=site)
-        assert edit2.can_add_core_uncontrolled_data(user, obj)
+        assert edit2.can_add_core_uncontrolled_data(user, site)
 
     @pytest.mark.parametrize("visibility", [Visibility.PUBLIC, Visibility.MEMBERS])
     @pytest.mark.django_db
     def test_assistant_blocked_for_non_team_data(self, visibility):
         site, user = get_site_with_member(Visibility.PUBLIC, Role.ASSISTANT)
-        obj = UncontrolledSiteContentFactory.build(site=site)
-        assert not edit2.can_add_core_uncontrolled_data(user, obj)
+        assert not edit2.can_add_core_uncontrolled_data(user, site)
 
     @pytest.mark.django_db
     def test_assistant_permitted_for_team_data(self):
         site, user = get_site_with_member(Visibility.TEAM, Role.ASSISTANT)
-        obj = UncontrolledSiteContentFactory.build(site=site)
-        assert edit2.can_add_core_uncontrolled_data(user, obj)
+        assert edit2.can_add_core_uncontrolled_data(user, site)
