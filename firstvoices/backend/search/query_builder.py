7ee682d1639abb6541e17183bc6d510189328bd2
Bronson Chapman <64934597+bronson-c@users.noreply.github.com>
2023-07-04 15:02:57 -0600

    FW-4418 Search Permissions (#246)
    * add search permission query
    
    * add tests
    
    * bonus change, add rebuild index to reset script
    
    * add changes from comments: Stronger warning, test and no-user-permissions changes
    
    * check if ES is running in reset script
    
    * pytest fixes

--- firstvoices/backend/search/query_builder.py
+++ firstvoices/backend/search/query_builder.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AnonymousUser
 from elasticsearch_dsl import Search
 
 from backend.search.utils.constants import VALID_DOCUMENT_TYPES
@@ -11,6 +12,7 @@ from backend.search.utils.query_builder_utils import (
     get_site_filter_query,
     get_starts_with_query,
     get_types_query,
+    get_view_permissions_filter,
 )
 
 
@@ -20,6 +22,7 @@ def get_search_object(indices):
 
 
 def get_search_query(
+    user=None,
     q=None,
     site_id=None,
     types=VALID_DOCUMENT_TYPES,
@@ -33,6 +36,14 @@ def get_search_query(
     indices = get_indices(types)
     search_object = get_search_object(indices)
     search_query = search_object.query()
+
+    # View permissions
+    if user is None:
+        user = AnonymousUser()
+
+    permissions_filter = get_view_permissions_filter(user)
+    if permissions_filter:
+        search_query = search_query.query(permissions_filter)
 
     # Adding search term and domain filter
     if q:
