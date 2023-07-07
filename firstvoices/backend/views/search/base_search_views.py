2b48c56b7aa537e071983cfc8aa104184a62a235
Bronson Chapman <64934597+bronson-c@users.noreply.github.com>
2023-07-07 16:44:52 -0600

    FW-4598 Handle invalid page and pageSize values in search Pagination (#273)
    * handle invalid seach pagination parameters
    
    * review change

--- firstvoices/backend/views/search/base_search_views.py
+++ firstvoices/backend/views/search/base_search_views.py
@@ -263,8 +263,12 @@ class BaseSearchViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
         """
         default_page_size = self.paginator.get_page_size(self.request)
 
-        page = int(self.request.GET.get("page", 1))
-        page_size = int(self.request.GET.get("pageSize", default_page_size))
+        page = self.paginator.override_invalid_number(self.request.GET.get("page", 1))
+
+        page_size = self.paginator.override_invalid_number(
+            self.request.GET.get("pageSize", default_page_size), default_page_size
+        )
+
         start = (page - 1) * page_size
 
         pagination_params = {
