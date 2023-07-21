ca9aaf12c473eb3df0bdc33048b1284e1b4175b3
Sahil Sharma <sahil.sharma071997@gmail.com>
2023-07-21 11:22:39 -0700

    added category import

---
+++ firstvoices/backend/resources/categories.py
@@ -0,0 +1,23 @@
+from import_export import fields
+
+from backend.models.category import Category
+from backend.models.constants import Visibility
+from backend.resources.base import BaseResource
+from backend.resources.utils.import_export_widgets import ChoicesWidget
+
+
+class CategoryResource(BaseResource):
+    visibility = fields.Field(
+        column_name="visibility",
+        widget=ChoicesWidget(Visibility.choices),
+        attribute="visibility",
+    )
+
+    class Meta:
+        model = Category
+
+
+class CategoryMigrationResource(CategoryResource):
+    def before_import(self, dataset, using_transactions, dry_run, **kwargs):
+        if not dry_run:
+            Category.objects.filter(site__id__in=dataset["site"]).delete()
