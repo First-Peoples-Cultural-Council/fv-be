3be5f2db803a170c023f687d2ceb6a5de459c0a3
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2023-07-27 09:44:29 -0700

    fw-4608, fix create_superuser function

--- firstvoices/jwt_auth/managers.py
+++ firstvoices/jwt_auth/managers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, id, email, password=None):
+    def create_user(self, email, password=None, sub=None):
         """
         Creates and saves a User with the given email and sub
         """
@@ -10,21 +10,21 @@ class UserManager(BaseUserManager):
         if not email:
             raise ValueError("Users must have an email address")
 
-        user = self.model(id=id, email=email)
+        user = self.model(email=email, sub=sub)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, id, email, password):
+    def create_superuser(self, email, password, sub=None):
         """
         Creates and saves a superuser with the given email and password
         """
         if not email:
             raise ValueError("Admins must have password")
 
-        user = self.create_user(id=email, email=email, password=password)
+        user = self.create_user(email=email, password=password, sub=sub)
 
         user.is_superuser = True
         user.is_staff = True
