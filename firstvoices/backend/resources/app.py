c3342771318a0c6e579ab0f248c10724fd74825c
Clarissa Forbes <clarissa@fpcc.ca>
2023-08-15 18:54:21 -0700

    import memberships and tests

---
+++ firstvoices/backend/resources/app.py
@@ -0,0 +1,32 @@
+from import_export import fields
+
+from backend.models.app import AppMembership
+from backend.models.constants import AppRole
+from backend.resources.base import BaseResource
+from backend.resources.utils.import_export_widgets import (
+    ChoicesWidget,
+    UserForeignKeyWidget,
+)
+
+
+class AppMembershipResource(BaseResource):
+    role = fields.Field(
+        column_name="role",
+        widget=ChoicesWidget(AppRole.choices),
+        attribute="role",
+    )
+    user = fields.Field(
+        column_name="user",
+        attribute="user",
+        widget=UserForeignKeyWidget(create=False),
+    )
+
+    class Meta:
+        model = AppMembership
+
+    def skip_row(self, instance, original, row, import_validation_errors=None):
+        """Don't create/overwrite app-level roles for users that already have one."""
+        user_has_role = AppMembership.objects.filter(user=instance.user).exists()
+        if user_has_role:
+            return True
+        return super().skip_row(instance, original, row, import_validation_errors)
