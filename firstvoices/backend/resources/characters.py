7f677c5da83854f027fd803fbdcedcfb44b3dc48
Sahil Sharma <sahil.sharma071997@gmail.com>
2023-08-22 16:14:11 -0700

    related media working

--- firstvoices/backend/resources/characters.py
+++ firstvoices/backend/resources/characters.py
@@ -1,13 +1,26 @@
 from import_export import fields
-from import_export.widgets import ForeignKeyWidget
+from import_export.widgets import ForeignKeyWidget, ManyToManyWidget
 
 from backend.models.characters import Character, CharacterVariant, IgnoredCharacter
 from backend.models.dictionary import DictionaryEntry, DictionaryEntryRelatedCharacter
+from backend.models.media import Audio, Video
 from backend.resources.base import BaseResource, SiteContentResource
 
 
 class CharacterResource(SiteContentResource):
     # add related media mapping
+    related_audio = fields.Field(
+        column_name="related_audio",
+        attribute="related_audio",
+        m2m_add=True,
+        widget=ManyToManyWidget(Audio, field="id"),
+    )
+    related_videos = fields.Field(
+        column_name="related_videos",
+        attribute="related_videos",
+        m2m_add=True,
+        widget=ManyToManyWidget(Video, "id"),
+    )
 
     class Meta:
         model = Character
