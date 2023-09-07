70719f0484c269f01b83b4a91a76ea528c174cdd
Clarissa Forbes <clarissa@fpcc.ca>
2023-09-07 15:09:51 -0700

    use default metadata for sitewidgetlists

--- firstvoices/backend/resources/site_homepage_widgets.py
+++ firstvoices/backend/resources/site_homepage_widgets.py
@@ -1,8 +1,8 @@
 from import_export import resources
-from jwt_auth.models import User
 
 from backend.models import Site
 from backend.models.widget import SiteWidget, SiteWidgetList, SiteWidgetListOrder
+from backend.resources.utils.import_export_widgets import UserForeignKeyWidget
 
 
 class SiteHomepageWidgetsResource(resources.ModelResource):
@@ -10,25 +10,18 @@ class SiteHomepageWidgetsResource(resources.ModelResource):
         model = Site
         fields = ("id",)
 
-    @staticmethod
-    def get_user_or_none(email):
-        try:
-            return User.objects.get(email=email)
-        except User.DoesNotExist:
-            return None
-
     def before_import_row(self, row, row_number=None, **kwargs):
         if row["homepage_widgets"] != "":
-            last_modified_by_user = self.get_user_or_none(row["last_modified_by"])
-            created_by_user = self.get_user_or_none(row["created_by"])
+            user_widget = UserForeignKeyWidget()
+            last_modified_by_user = user_widget.clean(value=row["last_modified_by"])
+            created_by_user = user_widget.clean(value=row["created_by"])
+
             site = Site.objects.get(id=row["id"])
             widgets_list = row["homepage_widgets"].split(",")
             site_widget_list = SiteWidgetList.objects.create(
                 site=site,
                 last_modified_by=last_modified_by_user,
-                last_modified=row["last_modified"],
                 created_by=created_by_user,
-                created=row["created"],
             )
             site.homepage = site_widget_list
             site.save()
@@ -39,7 +32,5 @@ class SiteHomepageWidgetsResource(resources.ModelResource):
                     site_widget_list=site_widget_list,
                     order=index,
                     last_modified_by=last_modified_by_user,
-                    last_modified=row["last_modified"],
                     created_by=created_by_user,
-                    created=row["created"],
                 )
