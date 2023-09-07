d221b7e21f87ca2311e75a15338f3e29d9a261db
Nolan Van Hell <nolan@fpcc.ca>
2023-09-07 15:50:35 -0700

    FW-4775 Add site homepage widget data migration (#453)
    * FW-4775 Add site homepage widget data migration
    
    * FW-4775 Update logging for site homepage
    
    * use default metadata for sitewidgetlists
    
    * futureproof type names in import log
    
    ---------
    
    Co-authored-by: Clarissa Forbes <clarissa@fpcc.ca>

--- firstvoices/backend/management/commands/import_csv_data.py
+++ firstvoices/backend/management/commands/import_csv_data.py
@@ -36,6 +36,7 @@ from backend.resources.media import (
     VideoResource,
 )
 from backend.resources.pages import SitePageResource
+from backend.resources.site_homepage_widgets import SiteHomepageWidgetsResource
 from backend.resources.sites import (
     MembershipResource,
     SiteMigrationResource,
@@ -126,6 +127,7 @@ def run_import():
         ("dict-entrylinks", DictionaryEntryLinkResource()),
         ("site-widgets", SiteWidgetResource()),
         ("widget-settings", WidgetSettingsResource()),
+        ("sites", SiteHomepageWidgetsResource()),
         ("pages", SitePageResource()),
         ("stories", StoryResource()),
         # more to be added
@@ -135,9 +137,8 @@ def run_import():
     unmatched_files = os.listdir(current_export_dir)
 
     for key, resource in import_resources:
-        logger.info(f"Importing {key} models...")
-
         # Parse files to import with this resource
+        logger.info(f"Importing from [{key}] CSV with {type(resource).__name__}...")
         matched_files = [f for f in unmatched_files if f.startswith(key)]
         unmatched_files = [f for f in unmatched_files if f not in matched_files]
 
