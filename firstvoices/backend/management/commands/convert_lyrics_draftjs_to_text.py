269cd2fa314bc9bb13f997744f5019e33b5b9c04
Nolan Van Hell <nolan@fpcc.ca>
2023-10-06 14:43:49 -0700

    Fix draftjs to text missing blocks key error

--- firstvoices/backend/management/commands/convert_lyrics_draftjs_to_text.py
+++ firstvoices/backend/management/commands/convert_lyrics_draftjs_to_text.py
@@ -20,19 +20,24 @@ def lyric_draftjs_to_text():
     for lyric in lyric_text_list:
         try:
             new_lyric_text = extract_plain_text(json.loads(lyric.text))
-            lyric.text = new_lyric_text
-            lyric.save()
+            if new_lyric_text is not None:
+                lyric.text = new_lyric_text
+                lyric.save()
         except JSONDecodeError:
             continue
 
     for lyric in lyric_translation_list:
         try:
             new_lyric_translation = extract_plain_text(json.loads(lyric.translation))
-            lyric.translation = new_lyric_translation
-            lyric.save()
+            if new_lyric_translation is not None:
+                lyric.translation = new_lyric_translation
+                lyric.save()
         except JSONDecodeError:
             continue
 
 
 def extract_plain_text(draftjs):
-    return delimiter.join([block["text"] for block in draftjs["blocks"]])
+    if "blocks" in draftjs:
+        return delimiter.join([block["text"] for block in draftjs["blocks"]])
+    else:
+        return None
