2dbe77c1c15fb3f404d9fd79f2bd18d4e2f40452
Bronson Chapman <bronsonchapman01@gmail.com>
2023-10-12 14:16:27 -0600

    add query optimization and fix typos

--- firstvoices/backend/views/join_request_views.py
+++ firstvoices/backend/views/join_request_views.py
@@ -10,7 +10,7 @@ from backend.views.base_views import FVPermissionViewSetMixin, SiteContentViewSe
 
 @extend_schema_view(
     list=extend_schema(
-        description=_("A list of categories associated with the specified site."),
+        description=_("A list of join requests associated with the specified site."),
         responses={
             200: OpenApiResponse(
                 description=doc_strings.success_200_list,
@@ -21,7 +21,7 @@ from backend.views.base_views import FVPermissionViewSetMixin, SiteContentViewSe
         },
     ),
     retrieve=extend_schema(
-        description=_("Details about a specific category."),
+        description=_("Details about a specific join request."),
         responses={
             200: OpenApiResponse(
                 description=doc_strings.success_200_detail,
@@ -47,7 +47,7 @@ class JoinRequestViewSet(
         site = self.get_validated_site()
         if site.count() > 0:
             return JoinRequest.objects.filter(site__slug=site[0].slug).select_related(
-                "site", "site__language", "created_by", "last_modified_by"
+                "site", "site__language", "created_by", "last_modified_by", "user"
             )
         else:
             return JoinRequest.objects.none()
