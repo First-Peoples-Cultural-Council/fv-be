1db9a73c61a0a53816dd74eef83bc769a6f44244
Bronson Chapman <bronsonchapman01@gmail.com>
2023-10-16 16:14:47 -0600

    add standard write tests to join reqest API

--- firstvoices/backend/tests/test_apis/test_join_request_api.py
+++ firstvoices/backend/tests/test_apis/test_join_request_api.py
@@ -1,12 +1,10 @@
 from backend.models import JoinRequest
 from backend.models.join_request import JoinRequestReason, JoinRequestStatus
 from backend.tests import factories
-from backend.tests.test_apis.base_api_test import (
-    BaseReadOnlyUncontrolledSiteContentApiTest,
-)
+from backend.tests.test_apis.base_api_test import BaseUncontrolledSiteContentApiTest
 
 
-class TestJoinRequestEndpoints(BaseReadOnlyUncontrolledSiteContentApiTest):
+class TestJoinRequestEndpoints(BaseUncontrolledSiteContentApiTest):
     """
     End-to-end tests that the join request endpoints have the expected behaviour.
     """
@@ -53,3 +51,54 @@ class TestJoinRequestEndpoints(BaseReadOnlyUncontrolledSiteContentApiTest):
 
     def get_expected_detail_response(self, instance, site):
         return self.get_expected_response(instance, site)
+
+    def get_valid_data(self, site=None):
+        return {
+            "user": factories.UserFactory().email,
+            "status": "pending",
+            "reason": "other",
+            "reason_note": "Test reason note",
+        }
+
+    def assert_updated_instance(self, expected_data, actual_instance):
+        assert actual_instance.status.get_status_display() == expected_data["status"]
+        assert actual_instance.reason.get_reason_display() == expected_data["reason"]
+        assert actual_instance.reason_note == expected_data["reason_note"]
+
+    def assert_update_response(self, expected_data, actual_response):
+        assert actual_response["status"] == expected_data["status"]
+        assert actual_response["reason"] == expected_data["reason"]
+        assert actual_response["reasonNote"] == expected_data["reason_note"]
+
+    def assert_created_instance(self, pk, data):
+        instance = JoinRequest.objects.get(pk=pk)
+        assert instance.status.get_status_display() == data["status"]
+        assert instance.reason.get_reason_display() == data["reason"]
+        assert instance.reason_note == data["reason_note"]
+
+    def assert_created_response(self, expected_data, actual_response):
+        assert actual_response["status"] == expected_data["status"]
+        assert actual_response["reason"] == expected_data["reason"]
+        assert actual_response["reasonNote"] == expected_data["reason_note"]
+
+    def create_original_instance_for_patch(self, site):
+        return factories.JoinRequestFactory(
+            site=site,
+            user=factories.UserFactory(),
+            status=JoinRequestStatus.PENDING,
+            reason=JoinRequestReason.OTHER,
+            reason_note="Test reason note",
+        )
+
+    def get_valid_patch_data(self, site=None):
+        return {
+            "status": JoinRequestStatus.APPROVED,
+        }
+
+    def add_related_objects(self, instance):
+        # no related objects to add
+        pass
+
+    def assert_related_objects_deleted(self, instance):
+        # no related objects to delete
+        pass
