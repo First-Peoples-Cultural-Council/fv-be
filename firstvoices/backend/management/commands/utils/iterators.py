44f1fd236ec95abdbc6ac9e00bfe13b3aad9cd30
Sahil Sharma <sahil.sharma071997@gmail.com>
2023-10-23 11:39:49 -0700

    Added logging instead of stdout for rebuild_index management command

--- firstvoices/backend/management/commands/utils/iterators.py
+++ firstvoices/backend/management/commands/utils/iterators.py
@@ -1,3 +1,5 @@
+import logging
+
 from backend.models import DictionaryEntry, Song, Story
 from backend.models.constants import Visibility
 from backend.models.media import Audio, Image, Video
@@ -33,36 +35,42 @@ def media_doc_generator(instance, media_type):
 
 
 def audio_iterator():
+    logger = logging.getLogger("rebuild_index")
+
     queryset = Audio.objects.all()
     for instance in queryset:
         try:
             yield media_doc_generator(instance, "audio").to_dict(True)
         except AttributeError:
-            print(
+            logger.warning(
                 f"Skipping document due to missing properties. object: audio, id: {instance.id}."
             )
             continue
 
 
 def image_iterator():
+    logger = logging.getLogger("rebuild_index")
+
     queryset = Image.objects.all()
     for instance in queryset:
         try:
             yield media_doc_generator(instance, "image").to_dict(True)
         except AttributeError:
-            print(
+            logger.warning(
                 f"Skipping document due to missing properties. object: image, id: {instance.id}."
             )
             continue
 
 
 def video_iterator():
+    logger = logging.getLogger("rebuild_index")
+
     queryset = Video.objects.all()
     for instance in queryset:
         try:
             yield media_doc_generator(instance, "video").to_dict(True)
         except AttributeError:
-            print(
+            logger.warning(
                 f"Skipping document due to missing properties. object: video, id: {instance.id}."
             )
             continue
