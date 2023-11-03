cf511bed988132ac4f6e9abc59efac48e39c706b
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2023-11-03 15:36:41 -0700

    fw-4886, move conftest to test_apis
    Use real media dimensions for media model tests

--- firstvoices/backend/tests/test_apis/conftest.py
+++ firstvoices/backend/tests/test_apis/conftest.py
@@ -7,20 +7,6 @@ from backend.models.media import ImageFile, VideoFile
 MOCK_MEDIA_DIMENSIONS = {"width": 100, "height": 100}
 
 
-@pytest.fixture(autouse=True, scope="session")
-def image_thumbnail_generation_does_nothing():
-    with patch("backend.models.media.Image._request_thumbnail_generation") as mocked:
-        mocked.return_value = None
-        yield
-
-
-@pytest.fixture(autouse=True, scope="session")
-def video_thumbnail_generation_does_nothing():
-    with patch("backend.models.media.Video._request_thumbnail_generation") as mocked:
-        mocked.return_value = None
-        yield
-
-
 @pytest.fixture(autouse=True)
 def mock_get_video_dimensions(mocker):
     mock_video_dimensions = mocker.patch.object(
