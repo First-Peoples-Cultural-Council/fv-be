67e8c4241a2311e5d4c10866cc8b3bcc0dd05bb7
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2023-11-07 15:26:38 -0800

    fw-4886, enable media api tests

--- firstvoices/backend/tests/conftest.py
+++ firstvoices/backend/tests/conftest.py
@@ -7,20 +7,42 @@ from backend.models.media import ImageFile, VideoFile
 MOCK_MEDIA_DIMENSIONS = {"width": 100, "height": 100}
 
 
-@pytest.fixture(autouse=True)
-def mock_get_video_dimensions(mocker):
-    mock_video_dimensions = mocker.patch.object(
-        VideoFile, "get_video_info", return_value=MOCK_MEDIA_DIMENSIONS
-    )
-    yield mock_video_dimensions
+@pytest.fixture(autouse=True, scope="session")
+def image_thumbnail_generation_does_nothing(request):
+    if "real_media_dimensions" not in request.keywords:
+        with patch(
+            "backend.models.media.Image._request_thumbnail_generation"
+        ) as mocked:
+            mocked.return_value = None
+            yield
+
+
+@pytest.fixture(autouse=True, scope="session")
+def video_thumbnail_generation_does_nothing(request):
+    if "real_media_dimensions" not in request.keywords:
+        with patch(
+            "backend.models.media.Video._request_thumbnail_generation"
+        ) as mocked:
+            mocked.return_value = None
+            yield
 
 
 @pytest.fixture(autouse=True)
-def mock_get_image_dimensions(mocker):
-    mock_image_dimensions = mocker.patch.object(
-        ImageFile, "get_image_dimensions", return_value=MOCK_MEDIA_DIMENSIONS
-    )
-    yield mock_image_dimensions
+def mock_get_video_dimensions(request, mocker):
+    if "real_media_dimensions" not in request.keywords:
+        mocker.patch.object(
+            VideoFile, "get_video_info", return_value=MOCK_MEDIA_DIMENSIONS
+        )
+        yield
+
+
+@pytest.fixture(autouse=True)
+def mock_get_image_dimensions(request, mocker):
+    if "real_media_dimensions" not in request.keywords:
+        mocker.patch.object(
+            ImageFile, "get_image_dimensions", return_value=MOCK_MEDIA_DIMENSIONS
+        )
+        yield
 
 
 @pytest.fixture(autouse=True, scope="session")
