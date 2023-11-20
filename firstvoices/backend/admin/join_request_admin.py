ce3e25dc8799935e25f2ab917afb73e32a6c18d4
Nolan Van Hell <nolan@fpcc.ca>
2023-11-20 15:02:13 -0800

    FW-5210 Order join request list view by reverse created date

--- firstvoices/backend/admin/join_request_admin.py
+++ firstvoices/backend/admin/join_request_admin.py
@@ -8,4 +8,5 @@ from .base_admin import BaseSiteContentAdmin
 @admin.register(JoinRequest)
 class JoinRequestAdmin(BaseSiteContentAdmin):
     list_display = ("user", "status") + BaseSiteContentAdmin.list_display
-    search_fields = ("user__email",)
+    search_fields = ("user__email", "site__title")
+    autocomplete_fields = ("site", "user")
