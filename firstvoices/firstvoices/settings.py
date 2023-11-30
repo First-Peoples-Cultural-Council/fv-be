8b410ce6721761678b2f9e0963b08d5aa9fa5574
Nolan Van Hell <nolan@fpcc.ca>
2023-11-29 17:08:38 -0800

    FW-5265 Cast Sentry environment variables to float

--- firstvoices/firstvoices/settings.py
+++ firstvoices/firstvoices/settings.py
@@ -279,11 +279,11 @@ sentry_sdk.init(
         "SENTRY_ENVIRONMENT"
     ),  # Sends information to this environment on the dashboard
     release=os.getenv("SENTRY_RELEASE"),  # Tags information with this release version
-    traces_sample_rate=os.getenv(
-        "SENTRY_TRACES_SAMPLE_RATE", 1.0
+    traces_sample_rate=float(
+        os.getenv("SENTRY_TRACES_SAMPLE_RATE", 1.0)
     ),  # The percentage of traces to send to sentry (min 0.0, max 1.0)
-    sample_rate=os.getenv(
-        "SENTRY_ERROR_SAMPLE_RATE", 1.0
+    sample_rate=float(
+        os.getenv("SENTRY_ERROR_SAMPLE_RATE", 1.0)
     ),  # The percentage of errors to send to sentry (min 0.0, max 1.0)
     send_default_pii=False,  # Disables the sending of personally identifiable information (see
     # https://docs.sentry.io/platforms/python/guides/django/data-collected/)
