e17723007d443eacef864c837d84e4de7b5ee7fd
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2024-02-09 11:03:20 -0800

    fw-5362, use generic indexing tasks in all signals

--- firstvoices/backend/search/signals/story_signals.py
+++ firstvoices/backend/search/signals/story_signals.py
@@ -2,27 +2,24 @@ from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 from backend.models.story import Story, StoryPage
-from backend.search.tasks import story_index_tasks
-from firstvoices.celery import link_error_handler
+from backend.search.indexing.story_index import StoryDocumentManager
+from backend.search.tasks.index_manager_tasks import (
+    request_remove_from_index,
+    request_sync_in_index,
+)
 
 
 @receiver(post_save, sender=Story)
 def sync_story_in_index(sender, instance, **kwargs):
-    story_index_tasks.sync_story_in_index.apply_async(
-        (instance.id,), link_error=link_error_handler.s()
-    )
+    request_sync_in_index(StoryDocumentManager, instance)
 
 
 @receiver(post_delete, sender=Story)
 def remove_story_from_index(sender, instance, **kwargs):
-    story_index_tasks.remove_story_from_index.apply_async(
-        (instance.id,), link_error=link_error_handler.s()
-    )
+    request_remove_from_index(StoryDocumentManager, instance)
 
 
 @receiver(post_delete, sender=StoryPage)
 @receiver(post_save, sender=StoryPage)
 def sync_story_pages_in_index(sender, instance, **kwargs):
-    story_index_tasks.sync_story_in_index.apply_async(
-        (instance.story.id,), link_error=link_error_handler.s()
-    )
+    request_sync_in_index(StoryDocumentManager, instance.story)
