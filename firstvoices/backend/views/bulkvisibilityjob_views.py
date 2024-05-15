1040510270925df9afe9099e50cc41424e82978d
Bronson Chapman <64934597+bronson-c@users.noreply.github.com>
2024-05-15 12:05:50 -0600

    FW 5683/5684 Queue and Run Bulk Visibility Jobs (#896)
    * add bulk_visibility_change_job
    
    * Add post API docs for bulk visibility
    
    * add search reindex to bv job
    
    * fix bulk vis signal
    
    * remove unnecessary code from bv signal
    
    * add basic bv task tests
    
    * fix exception testing and always revert search index pausing on exception
    
    * fix visibility signal, add create bvj api tests, renaming
    
    * add negative API tests
    
    * remove signal test
    
    * reduce duplication
    
    * add check for currently running bv jobs
    
    * replace visibility signal with function call
    
    * edits to bulk_change_visibility task
    
    * resume indexing before reindex

--- firstvoices/backend/views/bulkvisibilityjob_views.py
+++ firstvoices/backend/views/bulkvisibilityjob_views.py
@@ -1,11 +1,14 @@
+from django.db import transaction
 from drf_spectacular.utils import OpenApiResponse, extend_schema, extend_schema_view
 from rest_framework.viewsets import ModelViewSet
 
 from backend.models.jobs import BulkVisibilityJob
 from backend.serializers.job_serializers import BulkVisibilityJobSerializer
+from backend.tasks.visibility_tasks import bulk_change_visibility
 from backend.views import doc_strings
 from backend.views.api_doc_variables import id_parameter, site_slug_parameter
 from backend.views.base_views import FVPermissionViewSetMixin, SiteContentViewSetMixin
+from firstvoices.celery import link_error_handler
 
 
 @extend_schema_view(
@@ -30,6 +33,16 @@ from backend.views.base_views import FVPermissionViewSetMixin, SiteContentViewSe
             id_parameter,
         ],
     ),
+    create=extend_schema(
+        description="Create and queue a new bulk visibility job.",
+        responses={
+            201: BulkVisibilityJobSerializer,
+            400: OpenApiResponse(description=doc_strings.error_400_validation),
+            403: OpenApiResponse(description=doc_strings.error_403),
+            404: OpenApiResponse(description=doc_strings.error_404),
+        },
+        parameters=[site_slug_parameter],
+    ),
 )
 class BulkVisibilityJobViewSet(
     SiteContentViewSetMixin, FVPermissionViewSetMixin, ModelViewSet
@@ -38,7 +51,7 @@ class BulkVisibilityJobViewSet(
     API endpoint that allows bulk visibility jobs to be viewed.
     """
 
-    http_method_names = ["get"]
+    http_method_names = ["get", "post"]
     serializer_class = BulkVisibilityJobSerializer
 
     def get_queryset(self):
@@ -47,4 +60,14 @@ class BulkVisibilityJobViewSet(
             BulkVisibilityJob.objects.filter(site=site)
             .select_related("site", "created_by", "last_modified_by")
             .order_by("created")
+        )
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+
+        # Queue the bulk visibility change after model creation
+        transaction.on_commit(
+            lambda: bulk_change_visibility.apply_async(
+                (instance.id,), link_error=link_error_handler.s()
+            )
         )
