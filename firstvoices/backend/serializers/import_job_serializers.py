09a0e958e4ae2d5ea6c67ca9890358cb26c3b324
Sahil Sharma <sahil.sharma071997@gmail.com>
2024-05-22 10:00:59 -0700

    refactored import_job to not use signals for dry-run, and to return validationReport once dry-run is finished

--- firstvoices/backend/serializers/import_job_serializers.py
+++ firstvoices/backend/serializers/import_job_serializers.py
@@ -31,14 +31,9 @@ class ImportReportRowSerializer(serializers.ModelSerializer):
 
 
 class ImportReportSerializer(serializers.ModelSerializer):
-    total_rows = serializers.IntegerField(read_only=True)
-    diff_headers = serializers.ListField(child=serializers.CharField(), read_only=True)
-    rows = ImportReportRowSerializer(many=True, read_only=True)
-    totals = serializers.JSONField(read_only=True)
-
     class Meta:
         model = ImportJobReport
-        fields = ["total_rows", "diff_headers", "rows", "totals"]
+        fields = ["new_rows", "skipped_rows", "error_rows"]
 
 
 class ImportJobSerializer(CreateSiteContentSerializerMixin, BaseJobSerializer):
@@ -47,21 +42,20 @@ class ImportJobSerializer(CreateSiteContentSerializerMixin, BaseJobSerializer):
         validators=[SupportedFileType(mimetypes=["text/csv", "text/plain"])],
     )
     run_as_user = serializers.CharField(required=False)
-
     validation_task_id = serializers.CharField(read_only=True)
     validation_status = fields.EnumField(enum=JobStatus, read_only=True)
-    validation_result = ImportReportSerializer(read_only=True)
+    validation_report = ImportReportSerializer(read_only=True)
 
     class Meta:
         model = ImportJob
         fields = BaseJobSerializer.Meta.fields + (
             "title",
             "mode",
-            "validation_result",
             "run_as_user",
             "data",
             "validation_task_id",
             "validation_status",
+            "validation_report",
         )
 
     def validate(self, attrs):
