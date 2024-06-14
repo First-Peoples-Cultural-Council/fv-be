590a7f7167fa275a5f54eac1b424118f13ea0f0a
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2024-06-14 12:08:24 -0700

    fw-5935, move indexing pause check into main mtd rebuild function
    - and add some logging

--- firstvoices/backend/models/signals/mtd_signals.py
+++ firstvoices/backend/models/signals/mtd_signals.py
@@ -1,18 +1,28 @@
+import logging
+
 from django.db import transaction
 from django.db.models.signals import post_delete, post_save, pre_save
 from django.dispatch import receiver
 
 from backend.models import Category, DictionaryEntry
 from backend.models.constants import Visibility
-from backend.search.signals import indexing_signals_paused
+from backend.search.signals.signal_utils import is_indexing_paused
 from backend.tasks.build_mtd_export_format import build_index_and_calculate_scores
 from firstvoices.celery import link_error_handler
 
+LOGGER = logging.getLogger(__name__)
 
-def rebuild_mtd_index(site_slug):
+
+def rebuild_mtd_index(site):
+    if is_indexing_paused(site):
+        LOGGER.debug(
+            f"Skipping mtd index rebuild because indexing is paused for site [{site.slug}]"
+        )
+        return
+
     transaction.on_commit(
         lambda: build_index_and_calculate_scores.apply_async(
-            (site_slug,),
+            (site.slug,),
             link_error=link_error_handler.s(),
         )
     )
@@ -21,11 +31,10 @@ def rebuild_mtd_index(site_slug):
 @receiver(pre_save, sender=DictionaryEntry)
 def store_current_visibility(sender, instance, **kwargs):
     # Adding old visibility to check later in post_save signal, if entry exists in the db
-    if not indexing_signals_paused(instance.site):
-        dictionary_entry = DictionaryEntry.objects.filter(id=instance.id)
-        if len(dictionary_entry):
-            old_visibility = dictionary_entry[0].visibility
-            instance.old_visibility = old_visibility
+    dictionary_entry = DictionaryEntry.objects.filter(id=instance.id)
+    if len(dictionary_entry):
+        old_visibility = dictionary_entry[0].visibility
+        instance.old_visibility = old_visibility
 
 
 @receiver(post_save, sender=DictionaryEntry)
@@ -33,12 +42,11 @@ def store_current_visibility(sender, instance, **kwargs):
 def request_update_mtd_index(sender, instance, **kwargs):
     # Checking for both previous and current visibility
     # this covers the case when a word is changed from public to members/team
-    if not indexing_signals_paused(instance.site):
-        if (
-            hasattr(instance, "old_visibility")
-            and instance.old_visibility == Visibility.PUBLIC
-        ) or instance.visibility == Visibility.PUBLIC:
-            rebuild_mtd_index(instance.site.slug)
+    if (
+        hasattr(instance, "old_visibility")
+        and instance.old_visibility == Visibility.PUBLIC
+    ) or instance.visibility == Visibility.PUBLIC:
+        rebuild_mtd_index(instance.site.slug)
 
 
 @receiver(post_save, sender=Category)
