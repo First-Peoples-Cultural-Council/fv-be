d8e2ad2c3e43766c3111269d1fcbc53906cb0fa4
Bronson Chapman <bronsonchapman01@gmail.com>
2024-07-04 15:07:02 -0600

    add periodic mtd sync task

--- firstvoices/backend/apps.py
+++ firstvoices/backend/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django_celery_beat.models import IntervalSchedule, PeriodicTask
 
 
 class BackendConfig(AppConfig):
@@ -8,3 +9,15 @@ class BackendConfig(AppConfig):
     def ready(self):
         import backend.models.signals  # noqa F401
         import backend.search.signals  # noqa F401
+
+        # Create the PeriodicTask and schedule for the MTD export task if they don't exist
+        if not PeriodicTask.objects.filter(name="sync_mtd_index").exists():
+            schedule, created = IntervalSchedule.objects.get_or_create(
+                every=6, period=IntervalSchedule.HOURS
+            )
+
+            PeriodicTask.objects.create(
+                interval=schedule,
+                name="sync_mtd_index",
+                task="backend.tasks.check_sites_for_mtd_sync",
+            )
