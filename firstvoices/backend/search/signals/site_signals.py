9d5e7f76c2b05bf7f8d1373c3a14a606d74f1819
Bronson Chapman <bronsonchapman01@gmail.com>
2024-07-10 13:06:16 -0600

    remove site feature signal and add sync methods to API only

--- firstvoices/backend/search/signals/site_signals.py
+++ firstvoices/backend/search/signals/site_signals.py
@@ -1,10 +1,9 @@
-from django.db.models.signals import post_delete, post_save, pre_save
+from django.db.models.signals import post_delete, pre_save
 from django.dispatch import receiver
 
-from backend.models.sites import Site, SiteFeature
+from backend.models.sites import Site
 from backend.search.tasks.site_content_indexing_tasks import (
     remove_all_site_content_from_indexes,
-    sync_all_media_site_content_in_indexes,
     sync_all_site_content_in_indexes,
 )
 
@@ -31,13 +30,6 @@ def change_site_visibility(sender, instance, **kwargs):
 @receiver(post_delete, sender=Site)
 def remove_all_site_content(sender, instance, **kwargs):
     remove_all_site_content_from_indexes(instance)
-
-
-@receiver(post_save, sender=SiteFeature)
-@receiver(post_delete, sender=SiteFeature)
-def sync_site_features_in_media_indexes(sender, instance, **kwargs):
-    site = instance.site
-    sync_all_media_site_content_in_indexes(site)
 
 
 def indexing_signals_paused(site):
--- firstvoices/backend/views/site_feature_views.py
+++ firstvoices/backend/views/site_feature_views.py
@@ -2,6 +2,9 @@ from drf_spectacular.utils import OpenApiResponse, extend_schema, extend_schema_
 from rest_framework.viewsets import ModelViewSet
 
 from backend.models.sites import SiteFeature
+from backend.search.tasks.site_content_indexing_tasks import (
+    sync_all_media_site_content_in_indexes,
+)
 from backend.serializers.site_feature_serializers import SiteFeatureDetailSerializer
 from backend.views import doc_strings
 from backend.views.api_doc_variables import key_parameter, site_slug_parameter
@@ -37,7 +40,8 @@ from backend.views.base_views import FVPermissionViewSetMixin, SiteContentViewSe
         ],
     ),
     create=extend_schema(
-        description="Create a new feature flag for the site. Site feature keys cannot be changed after creation.",
+        description="Create a new feature flag for the site. Site feature keys cannot be changed after creation. "
+        "Triggers a reindex of all media content in the site.",
         responses={
             201: OpenApiResponse(
                 description=doc_strings.success_201,
@@ -50,7 +54,8 @@ from backend.views.base_views import FVPermissionViewSetMixin, SiteContentViewSe
         parameters=[site_slug_parameter],
     ),
     update=extend_schema(
-        description="Edit a feature flag for the specified site. Site feature keys cannot be changed after creation.",
+        description="Edit a feature flag for the specified site. Site feature keys cannot be changed after creation."
+        "Triggers a reindex of all media content in the site.",
         responses={
             200: OpenApiResponse(
                 description=doc_strings.success_200_edit,
@@ -66,7 +71,8 @@ from backend.views.base_views import FVPermissionViewSetMixin, SiteContentViewSe
         ],
     ),
     partial_update=extend_schema(
-        description="Edit a feature flag for the specified site. Any omitted fields will be unchanged.",
+        description="Edit a feature flag for the specified site. Any omitted fields will be unchanged."
+        "Triggers a reindex of all media content in the site.",
         responses={
             200: OpenApiResponse(
                 description=doc_strings.success_200_edit,
@@ -82,7 +88,8 @@ from backend.views.base_views import FVPermissionViewSetMixin, SiteContentViewSe
         ],
     ),
     destroy=extend_schema(
-        description="Delete a feature flag from the specified site.",
+        description="Delete a feature flag from the specified site. "
+        "Triggers a reindex of all media content in the site.",
         responses={
             204: OpenApiResponse(description=doc_strings.success_204_deleted),
             403: OpenApiResponse(description=doc_strings.error_403),
@@ -109,3 +116,21 @@ class SiteFeatureViewSet(
             "created_by",
             "last_modified_by",
         )
+
+    def perform_create(self, serializer):
+        # Once a site feature is created via the API, sync all media content in indexes
+        instance = serializer.save()
+        site = instance.site
+        sync_all_media_site_content_in_indexes(site)
+
+    def perform_update(self, serializer):
+        # Once a site feature is updated via the API, sync all media content in indexes
+        instance = serializer.save()
+        site = instance.site
+        sync_all_media_site_content_in_indexes(site)
+
+    def perform_destroy(self, instance):
+        # Once a site feature is deleted via the API, sync all media content in indexes
+        site = instance.site
+        instance.delete()
+        sync_all_media_site_content_in_indexes(site)
