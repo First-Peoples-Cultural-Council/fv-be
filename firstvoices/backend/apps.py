e5796cd7b8bcedff1b7457b03b54575be8d42888
Bronson Chapman <bronsonchapman01@gmail.com>
2024-07-15 16:24:29 -0600

    re-add search signal import

--- firstvoices/backend/apps.py
+++ firstvoices/backend/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BackendConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "backend"
+
+    def ready(self):
+        import backend.search.signals  # noqa F401
