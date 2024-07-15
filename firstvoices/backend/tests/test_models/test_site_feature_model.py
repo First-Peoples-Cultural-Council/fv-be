2d3d29f4309cbe9d82822765c9f572d285bed0f8
Bronson Chapman <bronsonchapman01@gmail.com>
2024-07-15 14:27:51 -0600

    change models tests to ensure async function is not run

--- firstvoices/backend/tests/test_models/test_site_feature_model.py
+++ firstvoices/backend/tests/test_models/test_site_feature_model.py
@@ -2,9 +2,12 @@ import pytest
 
 from backend.models.sites import SiteFeature
 from backend.tests.factories import SiteFactory
+from backend.tests.test_search_indexing.base_indexing_tests import (
+    TransactionOnCommitMixin,
+)
 
 
-class TestSiteFeatureModel:
+class TestSiteFeatureModel(TransactionOnCommitMixin):
     """
     Tests for the SiteFeature model.
     Primarily tests that the model does not trigger a media sync outside of the API.
@@ -22,18 +25,28 @@ class TestSiteFeatureModel:
 
     @pytest.mark.django_db
     def test_create_does_not_trigger_media_sync(self, site):
-        SiteFeature.objects.create(site=site, key="test_key", is_enabled=True)
+        with self.capture_on_commit_callbacks(execute=True):
+            SiteFeature.objects.create(site=site, key="test_key", is_enabled=True)
+
         assert self.mocked_func.call_count == 0
 
     @pytest.mark.django_db
     def test_update_does_not_trigger_media_sync(self, site):
-        feature = SiteFeature.objects.create(site=site, key="test_key", is_enabled=True)
-        feature.is_enabled = False
-        feature.save()
+        with self.capture_on_commit_callbacks(execute=True):
+            feature = SiteFeature.objects.create(
+                site=site, key="test_key", is_enabled=True
+            )
+            feature.is_enabled = False
+            feature.save()
+
         assert self.mocked_func.call_count == 0
 
     @pytest.mark.django_db
     def test_delete_does_not_trigger_media_sync(self, site):
-        feature = SiteFeature.objects.create(site=site, key="test_key", is_enabled=True)
-        feature.delete()
+        with self.capture_on_commit_callbacks(execute=True):
+            feature = SiteFeature.objects.create(
+                site=site, key="test_key", is_enabled=True
+            )
+            feature.delete()
+
         assert self.mocked_func.call_count == 0
