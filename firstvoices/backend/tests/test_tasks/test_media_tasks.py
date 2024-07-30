b9b5cfe3cfe2d6f614cba2177921fc490b2fd9f9
Sahil Sharma <sahil.sharma071997@gmail.com>
2024-07-30 16:36:02 -0700

    Added tests for thumbnail generation updating last modified

--- firstvoices/backend/tests/test_tasks/test_media_tasks.py
+++ firstvoices/backend/tests/test_tasks/test_media_tasks.py
@@ -1,6 +1,13 @@
 import pytest
 
-from backend.tests.factories import ImageFactory, SiteFactory, VideoFactory
+from backend.models.media import Image, Video
+from backend.tests.factories import (
+    ImageFactory,
+    ImageFileFactory,
+    SiteFactory,
+    VideoFactory,
+    VideoFileFactory,
+)
 
 
 class TestThumbnailGeneration:
@@ -11,10 +18,34 @@ class TestThumbnailGeneration:
     )
     def test_thumbnail_generation_started(self, model_factory, model, caplog):
         site = SiteFactory()
-        media_item = model_factory.create(site=site, title="Test media item")
+        media_item = model_factory.create(site=site)
 
         assert (
             f"Task started. Additional info: "
             f"model_name: {model}, instance_id: {media_item.id}." in caplog.text
         )
         assert "Task ended." in caplog.text
+
+    @pytest.mark.django_db
+    @pytest.mark.disable_thumbnail_mocks
+    @pytest.mark.parametrize(
+        "model, model_factory, model_file_factory",
+        [
+            (Image, ImageFactory, ImageFileFactory),
+            (Video, VideoFactory, VideoFileFactory),
+        ],
+    )
+    def test_thumbnail_generation_does_not_update_last_modified(
+        self, model, model_factory, model_file_factory
+    ):
+        site = SiteFactory()
+
+        media_file = model_file_factory.create()
+        media_item = model_factory.create(site=site, original=media_file)
+        original_last_modified = media_item.last_modified
+
+        media_item = model.objects.get(id=media_item.id)
+
+        new_last_modified = media_item.last_modified
+
+        assert new_last_modified == original_last_modified
