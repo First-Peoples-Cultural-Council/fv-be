bc932760d6ccd36734a8a8a968b560d70402bf07
Bronson Chapman <bronsonchapman01@gmail.com>
2024-08-12 15:33:36 -0600

    add detailed documentation to dictionary cleaup job and preview

--- firstvoices/backend/views/dictionary_cleanup_views.py
+++ firstvoices/backend/views/dictionary_cleanup_views.py
@@ -19,7 +19,8 @@ from firstvoices.celery import link_error_handler
 
 @extend_schema_view(
     list=extend_schema(
-        description="A list of all dictionary cleanup results for the specified site.",
+        description="A list of all dictionary cleanup job results for the specified site. "
+        "See the detail view for more information on specific fields.",
         responses={
             200: DictionaryCleanupJobSerializer,
             403: OpenApiResponse(description=doc_strings.error_403),
@@ -28,7 +29,10 @@ from firstvoices.celery import link_error_handler
         parameters=[site_slug_parameter],
     ),
     retrieve=extend_schema(
-        description="Details about a specific dictionary cleanup result.",
+        description="Details about a specific dictionary cleanup job. "
+        "Includes standard task fields: status, task ID, and error message as well as a results field. "
+        "The results field contains the changes in custom order and title for every updated entry "
+        "and the count of unknown characters currently on the site.",
         responses={
             200: DictionaryCleanupJobSerializer,
             403: OpenApiResponse(description=doc_strings.error_403),
@@ -40,7 +44,10 @@ from firstvoices.celery import link_error_handler
         ],
     ),
     create=extend_schema(
-        description="Create and queue a new dictionary cleanup job.",
+        description="Queue a new dictionary cleanup job and create a model instance to track the job. "
+        "Dictionary cleanup jobs update the custom order and title of dictionary entries "
+        "if changes have been made to a site's alphabet or confusable characters. "
+        "The job will also count and provide the number of unknown characters currently on the site.",
         responses={
             201: DictionaryCleanupJobSerializer,
             403: OpenApiResponse(description=doc_strings.error_403),
@@ -48,8 +55,23 @@ from firstvoices.celery import link_error_handler
         },
         parameters=[site_slug_parameter],
     ),
+    destroy=extend_schema(
+        description="Deletes a single dictionary cleanup job result for the specified site. "
+        "Any job result regardless of status can be deleted this way, "
+        "but it does not cancel a job if it is currently running.",
+        responses={
+            204: OpenApiResponse(description=doc_strings.success_204_deleted),
+            403: OpenApiResponse(description=doc_strings.error_403),
+            404: OpenApiResponse(description=doc_strings.error_404_missing_site),
+        },
+        parameters=[
+            site_slug_parameter,
+            id_parameter,
+        ],
+    ),
     clear=extend_schema(
-        description="Deletes all finished dictionary cleanup results for the specified site.",
+        description="Deletes all finished dictionary cleanup job results for the specified site. "
+        "This includes jobs with the status of complete, failed, or cancelled.",
         responses={
             204: OpenApiResponse(description=doc_strings.success_204_deleted),
             403: OpenApiResponse(description=doc_strings.error_403),
@@ -105,7 +127,9 @@ class DictionaryCleanupJobViewSet(
 
 @extend_schema_view(
     list=extend_schema(
-        description="A list of all dictionary cleanup preview results for the specified site.",
+        description="A list of all dictionary cleanup preview job results for the specified site. "
+        "Preview results are not applied to the site and are used to show the changes that would be made. "
+        "See the detail view for more information on specific fields.",
         responses={
             200: DictionaryCleanupPreviewJobSerializer,
             403: OpenApiResponse(description=doc_strings.error_403),
@@ -114,7 +138,11 @@ class DictionaryCleanupJobViewSet(
         parameters=[site_slug_parameter],
     ),
     retrieve=extend_schema(
-        description="Details about a specific dictionary cleanup preview result.",
+        description="Details about a specific dictionary cleanup preview job. "
+        "Includes standard task fields: status, task ID, and error message as well as a results field. "
+        "The results field contains the changes in custom order and title that would be made for "
+        "every entry if a dictionary cleanup job were to be run, "
+        "and the count of unknown characters currently on the site.",
         responses={
             200: DictionaryCleanupPreviewJobSerializer,
             403: OpenApiResponse(description=doc_strings.error_403),
@@ -126,7 +154,10 @@ class DictionaryCleanupJobViewSet(
         ],
     ),
     create=extend_schema(
-        description="Create and queue a new dictionary cleanup preview job.",
+        description="Create and queue a new dictionary cleanup preview job. "
+        "Preview jobs are used to show the changes that would be made by a dictionary cleanup job. "
+        "The preview job provides the same results as a dictionary cleanup job without actually "
+        "performing the changes in the database.",
         responses={
             202: DictionaryCleanupPreviewJobSerializer,
             403: OpenApiResponse(description=doc_strings.error_403),
@@ -134,8 +165,23 @@ class DictionaryCleanupJobViewSet(
         },
         parameters=[site_slug_parameter],
     ),
+    destroy=extend_schema(
+        description="Deletes a single dictionary cleanup preview job result for the specified site. "
+        "Any job regardless of status can be deleted this way, "
+        "but it does not cancel a job if it is currently running.",
+        responses={
+            204: OpenApiResponse(description=doc_strings.success_204_deleted),
+            403: OpenApiResponse(description=doc_strings.error_403),
+            404: OpenApiResponse(description=doc_strings.error_404_missing_site),
+        },
+        parameters=[
+            site_slug_parameter,
+            id_parameter,
+        ],
+    ),
     clear=extend_schema(
-        description="Deletes all finished dictionary cleanup preview results for the specified site.",
+        description="Deletes all finished dictionary cleanup preview jobs for the specified site. "
+        "This includes jobs with the status of complete, failed, or cancelled.",
         responses={
             204: OpenApiResponse(description=doc_strings.success_204_deleted),
             403: OpenApiResponse(description=doc_strings.error_403),
