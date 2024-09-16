a341453fb5dc86a85550b3806c9f56caa51c2e04
Sahil Sharma <sahil.sharma071997@gmail.com>
2024-09-16 11:19:58 -0700

    Revert "FW-5937 Update our log formatter (#1046)" (#1052)
    This reverts commit 3bbe4b86de95995ed259c517efe2287ed986f75e.

--- firstvoices/firstvoices/logging_utils.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-
-class DebugInfoLogLevelFilter(logging.Filter):
-    def filter(self, record):
-        # Only allow DEBUG + INFO logs
-        return record.levelno in [logging.DEBUG, logging.INFO]
