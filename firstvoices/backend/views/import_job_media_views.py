61d1f84613805daecebd8593c2ab710a7607025c
Sahil Sharma <sahil.sharma071997@gmail.com>
2024-11-06 14:50:02 -0800

    wip added list endpoint

--- firstvoices/backend/views/import_job_media_views.py
+++ firstvoices/backend/views/import_job_media_views.py
@@ -1,20 +1,22 @@
 from django.core.exceptions import PermissionDenied
 from django.http import Http404
-from rest_framework import mixins, parsers, viewsets
+from rest_framework import parsers, viewsets
 from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
 
 from backend.models import ImportJob
 from backend.models.media import SUPPORTED_FILETYPES, File, ImageFile, VideoFile
-from backend.views.base_views import SiteContentViewSetMixin
+from backend.permissions.utils import filter_by_viewable
+from backend.serializers.files_serializers import FileSerializer
+from backend.views.base_views import FVPermissionViewSetMixin, SiteContentViewSetMixin
 
 
 class ImportJobMediaViewSet(
-    SiteContentViewSetMixin, mixins.CreateModelMixin, viewsets.GenericViewSet
+    SiteContentViewSetMixin, FVPermissionViewSetMixin, viewsets.ModelViewSet
 ):
     http_method_names = ["get", "post"]
-    queryset = ""
-
+    # serializer_class = FileSerializer
+    # pagination_class = PageNumberPagination
     parser_classes = [parsers.MultiPartParser]
 
     def get_validated_import_job(self):
@@ -46,8 +48,33 @@ class ImportJobMediaViewSet(
 
         return filetype
 
-    def list(self, request, **kwargs):
-        return Response([])
+    def list(self, *args, **kwargs):
+        site = self.get_validated_site()
+        import_job = self.get_validated_import_job()
+
+        result = []
+
+        # Audio
+        # audio_queryset = File.objects.filter(site=site, import_job=import_job)
+        # audio_files = filter_by_viewable(self.request.user, audio_queryset)
+        # result.append(FileSerializer(audio_files).data)
+
+        # applying perms
+
+        # Image
+        image_queryset = ImageFile.objects.filter(site=site, import_job=import_job)
+        image_queryset = filter_by_viewable(self.request.user, image_queryset)
+        images = [FileSerializer(image).data for image in image_queryset]
+        result.extend(images)
+
+        # # Video
+        # queryset.append(VideoFile.objects.filter(
+        #     site=site, import_job=import_job
+        # ))
+
+        paginated_data = self.paginate_queryset(result)
+
+        return Response(data=paginated_data)
 
     def create(self, *args, **kwargs):
         user = self.request.user
