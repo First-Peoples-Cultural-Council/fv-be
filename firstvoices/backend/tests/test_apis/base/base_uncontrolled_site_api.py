54cfbe53484857338c5484a880eec8e724c805f3
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2025-05-22 16:20:33 -0700

    Add create, update, and patch tests

--- firstvoices/backend/tests/test_apis/base/base_uncontrolled_site_api.py
+++ firstvoices/backend/tests/test_apis/base/base_uncontrolled_site_api.py
@@ -396,6 +396,25 @@ class SiteContentCreateApiTestMixin:
         self.assert_created_response(data, response_data)
 
     @pytest.mark.django_db
+    def test_create_confirm_user(self):
+        site, _ = factories.get_site_with_app_admin(self.client, Visibility.PUBLIC)
+
+        data = self.get_valid_data(site)
+
+        response = self.client.post(
+            self.get_list_endpoint(site_slug=site.slug),
+            data=self.format_upload_data(data),
+            content_type=self.content_type,
+        )
+
+        response_data = json.loads(response.content)
+        user_email = _.email
+
+        assert response_data["createdBy"] == user_email
+        assert response_data["lastModifiedBy"] == user_email
+        assert response_data["systemLastModifiedBy"] == user_email
+
+    @pytest.mark.django_db
     def test_create_with_nulls_success_201(self):
         site, _ = factories.get_site_with_app_admin(self.client, Visibility.PUBLIC)
 
@@ -553,6 +572,18 @@ class SiteContentUpdateApiTestMixin:
         response_data = self.perform_successful_detail_request(instance, site, data)
         self.assert_updated_instance(data, self.get_updated_instance(instance))
         self.assert_update_response(data, response_data)
+
+    @pytest.mark.django_db
+    def test_update_confirm_user(self):
+        site, _ = factories.get_site_with_app_admin(self.client, Visibility.PUBLIC)
+        instance = self.create_minimal_instance(site=site, visibility=Visibility.PUBLIC)
+        data = self.get_valid_data(site)
+
+        response_data = self.perform_successful_detail_request(instance, site, data)
+        user_email = _.email
+
+        assert response_data["lastModifiedBy"] == user_email
+        assert response_data["systemLastModifiedBy"] == user_email
 
     @pytest.mark.django_db
     def test_update_with_nulls_success_200(self):
@@ -763,6 +794,26 @@ class SiteContentPatchApiTestMixin:
             data, self.get_updated_patch_instance(instance)
         )
         self.assert_update_patch_response(instance, data, response_data)
+
+    @pytest.mark.django_db
+    def test_patch_confirm_user(self):
+        site, _ = factories.get_site_with_app_admin(self.client, Visibility.PUBLIC)
+        instance = self.create_original_instance_for_patch(site=site)
+        data = self.get_valid_patch_data(site)
+
+        response = self.client.patch(
+            self.get_detail_endpoint(
+                key=self.get_lookup_key(instance), site_slug=site.slug
+            ),
+            data=self.format_upload_data(data),
+            content_type=self.content_type,
+        )
+
+        response_data = json.loads(response.content)
+        user_email = _.email
+
+        assert response_data["lastModifiedBy"] == user_email
+        assert response_data["systemLastModifiedBy"] == user_email
 
 
 class BaseReadOnlyUncontrolledSiteContentApiTest(
