f6a1fc9c96fb280e396ac0ae8f2a856d757786cb
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2025-06-19 14:51:21 -0700

    Add update, patch, and delete to views

--- firstvoices/backend/views/membership_views.py
+++ firstvoices/backend/views/membership_views.py
@@ -5,6 +5,7 @@ from rest_framework.viewsets import ModelViewSet
 from backend.models import Membership
 from backend.serializers.membership_serializers import MembershipDetailSerializer
 from backend.views import doc_strings
+from backend.views.api_doc_variables import id_parameter, site_slug_parameter
 from backend.views.base_views import FVPermissionViewSetMixin, SiteContentViewSetMixin
 
 
@@ -19,6 +20,7 @@ from backend.views.base_views import FVPermissionViewSetMixin, SiteContentViewSe
             403: OpenApiResponse(description=doc_strings.error_403_site_access_denied),
             404: OpenApiResponse(description=doc_strings.error_404_missing_site),
         },
+        parameters=[site_slug_parameter],
     ),
     retrieve=extend_schema(
         description=_("Details about a specific membership."),
@@ -30,6 +32,69 @@ from backend.views.base_views import FVPermissionViewSetMixin, SiteContentViewSe
             403: OpenApiResponse(description=doc_strings.error_403),
             404: OpenApiResponse(description=doc_strings.error_404),
         },
+        parameters=[
+            site_slug_parameter,
+            id_parameter,
+        ],
+    ),
+    create=extend_schema(
+        description=_("Add a membership."),
+        responses={
+            201: OpenApiResponse(
+                description=doc_strings.success_201, response=MembershipDetailSerializer
+            ),
+            400: OpenApiResponse(description=doc_strings.error_400_validation),
+            403: OpenApiResponse(description=doc_strings.error_403),
+            404: OpenApiResponse(description=doc_strings.error_404_missing_site),
+        },
+        parameters=[site_slug_parameter],
+    ),
+    update=extend_schema(
+        description=_("Edit a membership."),
+        responses={
+            200: OpenApiResponse(
+                description=doc_strings.success_200_edit,
+                response=MembershipDetailSerializer,
+            ),
+            400: OpenApiResponse(description=doc_strings.error_400_validation),
+            403: OpenApiResponse(description=doc_strings.error_403),
+            404: OpenApiResponse(description=doc_strings.error_404),
+        },
+        parameters=[
+            site_slug_parameter,
+            id_parameter,
+        ],
+    ),
+    partial_update=extend_schema(
+        description=_("Edit a membership. Any omitted fields will be unchanged."),
+        responses={
+            200: OpenApiResponse(
+                description=doc_strings.success_200_edit,
+                response=MembershipDetailSerializer,
+            ),
+            400: OpenApiResponse(description=doc_strings.error_400_validation),
+            403: OpenApiResponse(description=doc_strings.error_403),
+            404: OpenApiResponse(description=doc_strings.error_404),
+        },
+        parameters=[
+            site_slug_parameter,
+            id_parameter,
+        ],
+    ),
+    destroy=extend_schema(
+        description=_("Delete a membership."),
+        responses={
+            204: OpenApiResponse(
+                description=doc_strings.success_204_deleted,
+            ),
+            400: OpenApiResponse(description=doc_strings.error_400_validation),
+            403: OpenApiResponse(description=doc_strings.error_403),
+            404: OpenApiResponse(description=doc_strings.error_404),
+        },
+        parameters=[
+            site_slug_parameter,
+            id_parameter,
+        ],
     ),
 )
 class MembershipViewSet(
@@ -40,7 +105,6 @@ class MembershipViewSet(
     """
 
     serializer_class = MembershipDetailSerializer
-    http_method_names = ["get"]
 
     def get_queryset(self):
         site = self.get_validated_site()
