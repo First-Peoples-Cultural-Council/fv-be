c418bcc128405c00a2767570d2083665ab4dd307
Sahil Sharma <sahil@fpcc.ca>
2025-07-17 10:17:40 -0700

    Added teardown method, and used factories

--- firstvoices/backend/integration_tests/search/test_data_search.py
+++ firstvoices/backend/integration_tests/search/test_data_search.py
@@ -4,7 +4,7 @@ from rest_framework.test import APIClient
 
 from backend.models import DictionaryEntry, Site
 from backend.models.constants import AppRole, Visibility
-from backend.tests.factories import get_app_admin
+from backend.tests.factories import DictionaryEntryFactory, get_app_admin
 
 
 @pytest.mark.django_db(transaction=True)
@@ -20,6 +20,17 @@ class TestDataSearch:
         )
         self.site.save()
 
+    def teardown_method(self):
+        DictionaryEntry.objects.filter(site=self.site).delete()
+
+    def get_search_response(self, query):
+        url = reverse(
+            "api:site-search-list", current_app="backend", args=[self.site.slug]
+        )
+        response = self.client.get(f"{url}?q={query}")
+        assert response.status_code == 200
+        return response.json()
+
     def test_exact_match(self):
         """
         Base case test to verify if the server is up and running.
@@ -28,82 +39,59 @@ class TestDataSearch:
         # If there are any entries from previous tests
         DictionaryEntry.objects.filter(site=self.site).delete()
 
-        entry_fish = DictionaryEntry(
+        entry_fish = DictionaryEntryFactory.create(
             site=self.site,
             visibility=Visibility.PUBLIC,
             title="Fish",
             created_by=self.admin_user,
             last_modified_by=self.admin_user,
         )
-        entry_fish.save()
-        entry_mountain = DictionaryEntry(
+        DictionaryEntryFactory.create(
             site=self.site,
             visibility=Visibility.PUBLIC,
             title="Mountain",
             created_by=self.admin_user,
             last_modified_by=self.admin_user,
         )
-        entry_mountain.save()
-        entry_apple = DictionaryEntry(
+        DictionaryEntryFactory.create(
             site=self.site,
             visibility=Visibility.PUBLIC,
             title="Apple",
             created_by=self.admin_user,
             last_modified_by=self.admin_user,
         )
-        entry_apple.save()
 
-        url = reverse(
-            "api:site-search-list", current_app="backend", args=[self.site.slug]
-        )
-        response = self.client.get(url + "?q=fish")
-        assert response.status_code == 200
-
-        response = response.json()
+        response = self.get_search_response("fish")
         assert response["count"] == 1
         assert response["results"][0]["entry"]["id"] == str(entry_fish.id)
-
-        # Clean-up since transaction=True so all entries get stored in the db
-        entry_fish.delete()
-        entry_mountain.delete()
-        entry_apple.delete()
 
     def test_substring_match(self):
         # If there are any entries from previous tests
         DictionaryEntry.objects.filter(site=self.site).delete()
 
-        entry_fishing = DictionaryEntry(
+        entry_fishing = DictionaryEntryFactory.create(
             site=self.site,
             visibility=Visibility.PUBLIC,
             title="Fishing",
             created_by=self.admin_user,
             last_modified_by=self.admin_user,
         )
-        entry_fishing.save()
-        entry_fisherman = DictionaryEntry(
+        entry_fisherman = DictionaryEntryFactory.create(
             site=self.site,
             visibility=Visibility.PUBLIC,
             title="Fisherman",
             created_by=self.admin_user,
             last_modified_by=self.admin_user,
         )
-        entry_fisherman.save()
-        entry_apple = DictionaryEntry(
+        DictionaryEntryFactory.create(
             site=self.site,
             visibility=Visibility.PUBLIC,
             title="Apple",
             created_by=self.admin_user,
             last_modified_by=self.admin_user,
         )
-        entry_apple.save()
 
-        url = reverse(
-            "api:site-search-list", current_app="backend", args=[self.site.slug]
-        )
-        response = self.client.get(url + "?q=fish")
-        assert response.status_code == 200
-
-        response = response.json()
+        response = self.get_search_response("fish")
         assert response["count"] == 2
 
         response_id_list = [
@@ -113,39 +101,31 @@ class TestDataSearch:
         assert str(entry_fishing.id) in response_id_list
         assert str(entry_fisherman.id) in response_id_list
 
-        # Clean-up since transaction=True so all entries get stored in the db
-        entry_fishing.delete()
-        entry_fisherman.delete()
-        entry_apple.delete()
-
     def test_fuzzy_match(self):
         # If there are any entries from previous tests
         DictionaryEntry.objects.filter(site=self.site).delete()
 
-        entry_fish = DictionaryEntry(
+        entry_fish = DictionaryEntryFactory.create(
             site=self.site,
             visibility=Visibility.PUBLIC,
             title="Fish",
             created_by=self.admin_user,
             last_modified_by=self.admin_user,
         )
-        entry_fish.save()
-        entry_dish = DictionaryEntry(
+        entry_dish = DictionaryEntryFactory.create(
             site=self.site,
             visibility=Visibility.PUBLIC,
             title="dish",
             created_by=self.admin_user,
             last_modified_by=self.admin_user,
         )
-        entry_dish.save()
-        entry_apple = DictionaryEntry(
+        DictionaryEntryFactory.create(
             site=self.site,
             visibility=Visibility.PUBLIC,
             title="Apple",
             created_by=self.admin_user,
             last_modified_by=self.admin_user,
         )
-        entry_apple.save()
 
         url = reverse(
             "api:site-search-list", current_app="backend", args=[self.site.slug]
@@ -163,39 +143,31 @@ class TestDataSearch:
         assert str(entry_fish.id) in response_id_list
         assert str(entry_dish.id) in response_id_list
 
-        # Clean-up since transaction=True so all entries get stored in the db
-        entry_fish.delete()
-        entry_dish.delete()
-        entry_apple.delete()
-
     def test_relevance_order(self):
         # If there are any entries from previous tests
         DictionaryEntry.objects.filter(site=self.site).delete()
 
-        entry_fish = DictionaryEntry(
+        DictionaryEntryFactory.create(
             site=self.site,
             visibility=Visibility.PUBLIC,
             title="Fish",
             created_by=self.admin_user,
             last_modified_by=self.admin_user,
         )
-        entry_fish.save()
-        entry_fishing = DictionaryEntry(
+        DictionaryEntryFactory.create(
             site=self.site,
             visibility=Visibility.PUBLIC,
             title="Fishing",
             created_by=self.admin_user,
             last_modified_by=self.admin_user,
         )
-        entry_fishing.save()
-        entry_dish = DictionaryEntry(
+        DictionaryEntryFactory.create(
             site=self.site,
             visibility=Visibility.PUBLIC,
             title="Dish",
             created_by=self.admin_user,
             last_modified_by=self.admin_user,
         )
-        entry_dish.save()
 
         url = reverse(
             "api:site-search-list", current_app="backend", args=[self.site.slug]
@@ -209,8 +181,3 @@ class TestDataSearch:
         results = [r["entry"]["title"] for r in response["results"]]
 
         assert results.index("Fish") < results.index("Fishing") < results.index("Dish")
-
-        # Clean up
-        entry_fish.delete()
-        entry_fishing.delete()
-        entry_dish.delete()
