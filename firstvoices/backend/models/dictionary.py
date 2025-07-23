3ea5c971ea967c5972caa9966857fd5a22547f61
Sahil Sharma <sahil@fpcc.ca>
2025-07-23 10:37:57 -0700

    Removed null flag from FK relation for external system on dictionary entry

--- firstvoices/backend/models/dictionary.py
+++ firstvoices/backend/models/dictionary.py
@@ -161,7 +161,6 @@ class DictionaryEntry(AudienceMixin, RelatedMediaMixin, BaseControlledSiteConten
         ExternalDictionaryEntrySystem,
         on_delete=models.SET_NULL,
         blank=True,
-        null=True,
     )
     external_system_entry_id = models.CharField(
         max_length=DEFAULT_TITLE_LENGTH, blank=True, null=True
