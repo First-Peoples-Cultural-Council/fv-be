770185ff4130affbb89324de0f878648d7e718bb
Sahil Sharma <sahil@fpcc.ca>
2025-09-05 13:18:39 -0700

    Added serializers for many-to-many and audience fields

--- firstvoices/backend/serializers/export_serializers.py
+++ firstvoices/backend/serializers/export_serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from backend.models import DictionaryEntry
 from backend.serializers.base_serializers import ReadOnlyVisibilityFieldMixin
+from backend.serializers.fields import CommaSeparatedIDsField, InvertedBooleanField
 from backend.serializers.search_result_serializers import (
     SearchResultPrefetchMixin,
     SearchResultSerializer,
@@ -13,9 +14,13 @@ class DictionaryEntryExportResultSerializer(
 ):
     part_of_speech = serializers.StringRelatedField()
     categories = serializers.StringRelatedField(many=True)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    include_in_games = InvertedBooleanField(source="exclude_from_games", read_only=True)
+    include_on_kids_site = InvertedBooleanField(
+        source="exclude_from_kids", read_only=True
+    )
+    audio_ids = CommaSeparatedIDsField(source="related_audio", read_only=True)
+    video_ids = CommaSeparatedIDsField(source="related_videos", read_only=True)
+    image_ids = CommaSeparatedIDsField(source="related_images", read_only=True)
 
     class Meta:
         model = DictionaryEntry
@@ -30,14 +35,14 @@ class DictionaryEntryExportResultSerializer(
             "acknowledgements",
             "alternate_spellings",
             "pronunciations",
-            "related_audio",
-            "related_images",
-            "related_videos",
+            "audio_ids",
+            "video_ids",
+            "image_ids",
             "part_of_speech",
             "related_video_links",
             "related_dictionary_entries",
-            "exclude_from_games",
-            "exclude_from_kids",
+            "include_in_games",
+            "include_on_kids_site",
         )
 
 
