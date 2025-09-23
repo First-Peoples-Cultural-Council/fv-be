db5feea29bf7bf6c632064018643194299b9d9f8
Sahil Sharma <sahil@fpcc.ca>
2025-09-23 12:17:53 -0700

    Updated error messages for batch validation, lowercased all columns before they go in for media validation

--- firstvoices/backend/tasks/import_job_tasks.py
+++ firstvoices/backend/tasks/import_job_tasks.py
@@ -67,8 +67,11 @@ def clean_csv(data, missing_uploaded_media=[], missing_referenced_media=[]):
     cleaned_data.headers = [header.lower() for header in cleaned_data.headers]
 
     # Remove rows that have missing media
-    rows_to_delete = list({(obj["idx"] - 1) for obj in missing_uploaded_media})
-    rows_to_delete += list({(obj["idx"] - 1) for obj in missing_referenced_media})
+    missing_media_row_idx = [(obj["idx"] - 1) for obj in missing_uploaded_media]
+    missing_referenced_media_row_idx = [
+        (obj["idx"] - 1) for obj in missing_referenced_media
+    ]
+    rows_to_delete = list({*missing_media_row_idx, *missing_referenced_media_row_idx})
     rows_to_delete.sort(reverse=True)
     for row_index in rows_to_delete:
         del cleaned_data[row_index]
@@ -120,7 +123,8 @@ def generate_report(
             report,
             row_number=missing_media_row["idx"],
             errors=[
-                f"Media file missing in uploaded files: {missing_media_row['filename']}."
+                f"Media file missing in uploaded files: "
+                f"{missing_media_row['filename']}, column: {missing_media_row['column']}."
             ],
         )
 
@@ -131,7 +135,8 @@ def generate_report(
             report,
             row_number=missing_media_id_row["idx"],
             errors=[
-                f"Referenced media not found for ID: {missing_media_id_row['id']}."
+                f"Referenced media not found for "
+                f"ID: {missing_media_id_row['id']} in column: {missing_media_id_row['column']}."
             ],
         )
 
@@ -335,6 +340,8 @@ def get_missing_uploaded_media(data, import_job):
     ]
 
     for field in media_fields:
+        field = field.lower()
+        data.headers = [h.lower() for h in data.headers]
         if field not in data.headers:
             continue
         for idx, filename in enumerate(data[field]):
@@ -345,7 +352,9 @@ def get_missing_uploaded_media(data, import_job):
             if valid_filename not in associated_filenames:
                 # If a filename is provided, but the file cannot be found in the associated files
                 # add an error row for that in missing media
-                missing_media.append({"idx": idx + 1, "filename": filename})
+                missing_media.append(
+                    {"idx": idx + 1, "filename": filename, "column": field}
+                )
 
     return missing_media
 
