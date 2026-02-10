b049c412745a59abb432bdf0772fe5aa970c296f
Guy McAuliffe <guy@fpcc.ca>
2026-02-10 14:05:59 -0800

    Add export_params to export job model and serializer

--- firstvoices/backend/serializers/export_job_serializers.py
+++ firstvoices/backend/serializers/export_job_serializers.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+
 from backend.models.jobs import ExportJob
 from backend.serializers.base_serializers import CreateSiteContentSerializerMixin
 from backend.serializers.files_serializers import FileSerializer
@@ -6,7 +8,11 @@ from backend.serializers.job_serializers import BaseJobSerializer
 
 class ExportJobSerializer(CreateSiteContentSerializerMixin, BaseJobSerializer):
     export_csv = FileSerializer(read_only=True)
+    export_params = serializers.JSONField(read_only=True)
 
     class Meta:
         model = ExportJob
-        fields = BaseJobSerializer.Meta.fields + ("export_csv",)
+        fields = BaseJobSerializer.Meta.fields + (
+            "export_csv",
+            "export_params",
+        )
