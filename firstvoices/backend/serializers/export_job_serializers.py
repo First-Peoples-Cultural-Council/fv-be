f577fc67b3e6109a483d8f7b759e829464f126e1
Bronson Chapman <bronson@fpcc.ca>
2026-07-20 15:49:14 -0700

    Add readable representations to export params list

--- firstvoices/backend/serializers/export_job_serializers.py
+++ firstvoices/backend/serializers/export_job_serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from backend.models.constants import MAX_EXPORT_JOBS
+from backend.models import Category, Person
+from backend.models.constants import MAX_EXPORT_JOBS, Visibility
 from backend.models.jobs import ExportJob, JobStatus
 from backend.serializers.base_serializers import CreateSiteContentSerializerMixin
 from backend.serializers.files_serializers import FileSerializer
@@ -27,6 +28,37 @@ class ExportJobSerializer(CreateSiteContentSerializerMixin, BaseJobSerializer):
                 "Please delete completed jobs that you no longer need to allow new export jobs to be created."
             )
         return attrs
+
+    def to_representation(self, obj):
+        # ensure that user readable params are displayed rather than ids/enums
+        representation = super().to_representation(obj)
+        export_params = representation["export_params"]
+        if not export_params:
+            return representation
+
+        # Get category title, replace category ID in representation only
+        if export_params["category_id"]:
+            try:
+                category = Category.objects.get(id=export_params["category_id"])
+            except Category.DoesNotExist:
+                category = None
+            export_params.pop("category_id", None)
+            export_params["category"] = category.title if category else ""
+
+        # Get speaker names
+        if export_params["speakers"]:
+            export_speakers = Person.objects.filter(id__in=export_params["speakers"])
+            export_params["speakers"] = [speaker.name for speaker in export_speakers]
+
+        # Get visibility labels
+        if export_params["visibility"]:
+            export_params["visibility"] = [
+                Visibility(visibility).label
+                for visibility in export_params["visibility"]
+            ]
+
+        representation["export_params"] = export_params
+        return representation
 
     class Meta:
         model = ExportJob
