1d0ed8974522bfe08f9a82bcce28bdb8079007b5
Bronson Chapman <64934597+bronson-c@users.noreply.github.com>
2026-08-21 21:39:04 +0000

    add export job task tests (#1766)

--- firstvoices/backend/tests/test_tasks/test_export_job_tasks.py
+++ firstvoices/backend/tests/test_tasks/test_export_job_tasks.py
@@ -1,3 +1,4 @@
+import uuid
 from datetime import timedelta
 from unittest.mock import patch
 
@@ -5,15 +6,83 @@ import pytest
 from django.db import connection
 
 from backend.models.files import File
-from backend.models.jobs import ExportJob
+from backend.models.jobs import ExportJob, JobStatus
 from backend.tasks.constants import ASYNC_TASK_END_TEMPLATE, ASYNC_TASK_START_TEMPLATE
-from backend.tasks.export_job_tasks import delete_old_exports
+from backend.tasks.export_job_tasks import delete_old_exports, generate_export_csv
 from backend.tests import factories
+from backend.tests.test_tasks.base_task_test import IgnoreTaskResultsMixin
 
 
 @pytest.mark.django_db
-class TestDeleteOldExportsTask:
+class TestExportJob(IgnoreTaskResultsMixin):
+    TASK = generate_export_csv
+
+    def get_valid_task_args(self):
+        return (uuid.uuid4(),)
+
+    def assert_caplog_text(self, caplog, job_id):
+        assert ASYNC_TASK_START_TEMPLATE % f"ExportJob id: {job_id}" in caplog.text
+        assert ASYNC_TASK_END_TEMPLATE in caplog.text
+
+    def test_generate_export_csv_invalid_id(self, caplog):
+        invalid_id = uuid.uuid4()
+        with pytest.raises(ExportJob.DoesNotExist):
+            generate_export_csv(str(invalid_id))
+
+        assert ASYNC_TASK_START_TEMPLATE % f"ExportJob id: {invalid_id}" in caplog.text
+
+    @pytest.mark.parametrize("status", [JobStatus.STARTED, JobStatus.COMPLETE])
+    def test_generate_export_csv_invalid_status(self, caplog, status):
+        export_job = factories.ExportJobFactory.create(status=status)
+        generate_export_csv(str(export_job.id))
+
+        self.assert_caplog_text(caplog, export_job.id)
+        assert (
+            "This job could not be started as it is either already running or completed."
+            in caplog.text
+        )
+
+        export_job.refresh_from_db()
+        assert export_job.status == JobStatus.FAILED
+
+    def test_generate_export_csv_no_results(self, caplog):
+        export_job = factories.ExportJobFactory.create()
+        generate_export_csv(str(export_job.id))
+
+        self.assert_caplog_text(caplog, export_job.id)
+        assert (
+            f"No results found for the export job with id {export_job.id}"
+            in caplog.text
+        )
+
+        export_job.refresh_from_db()
+        assert export_job.status == JobStatus.CANCELLED
+        assert export_job.export_csv is None
+
+    def test_generate_export_csv_exception(self, caplog):
+        export_job = factories.ExportJobFactory.create()
+        with patch(
+            "backend.tasks.export_job_tasks.generate_export",
+            side_effect=Exception("Mocked exception"),
+        ):
+            generate_export_csv(str(export_job.id))
+
+        self.assert_caplog_text(caplog, export_job.id)
+        assert (
+            f"An error occurred while generating export for ExportJob id: {export_job.id}. "
+            f"Error: Mocked exception."
+        ) in caplog.text
+
+        export_job.refresh_from_db()
+        assert export_job.status == JobStatus.FAILED
+
+
+@pytest.mark.django_db
+class TestDeleteOldExportsTask(IgnoreTaskResultsMixin):
     TASK = delete_old_exports
+
+    def get_valid_task_args(self):
+        return None
 
     def get_export_job_with_csv(self):
         return factories.ExportJobFactory.create(
