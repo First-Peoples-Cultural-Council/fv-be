b82f7dd85e0dad7ba493930c1620bb7c76f879c4
Bronson Chapman <bronson@fpcc.ca>
2026-09-16 12:23:35 -0700

    add ignore task results mixin to import tasks

--- firstvoices/backend/tests/test_tasks/base_task_test.py
+++ firstvoices/backend/tests/test_tasks/base_task_test.py
@@ -2,6 +2,10 @@ from unittest import mock
 
 import pytest
 
+from backend.models.import_jobs import ImportJobStatus
+from backend.tests import factories
+from backend.tests.utils import get_sample_file
+
 
 @pytest.mark.django_db
 class IgnoreTaskResultsMixin:
@@ -23,3 +27,33 @@ class IgnoreTaskResultsMixin:
             args=task_args,
         )
         mock_store_result.assert_not_called()
+
+
+@pytest.mark.django_db
+class IgnoreTaskResultsImportMixin(IgnoreTaskResultsMixin):
+    def get_import_job(
+        self,
+        file,
+        status=ImportJobStatus.ACCEPTED,
+        validation_status=ImportJobStatus.COMPLETE,
+    ):
+        self.import_job = factories.ImportJobFactory(
+            site=self.site,
+            run_as_user=self.user,
+            data=file,
+            status=status,
+            validation_status=validation_status,
+        )
+        return self.import_job
+
+    def get_valid_task_args(self):
+        if not hasattr(self, "import_job"):
+            file_content = get_sample_file(
+                file_dir=self.CSV_FILES_DIR,
+                filename="minimal.csv",
+                mimetype=self.MIMETYPE,
+            )
+            file = factories.FileFactory(content=file_content)
+            self.import_job = self.get_import_job(file=file)
+
+        return (str(self.import_job.id),)
